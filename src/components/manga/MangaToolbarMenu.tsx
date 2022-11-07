ff5163ac6b50e9422c971fce5e9c2c786191f6a1
Valter Martinek <martinek@freevision.sk>
2022-11-07 21:48:14 +0100

    Add icon buttons to desktop sizes of manga toolbar menu

--- src/components/manga/MangaToolbarMenu.tsx
+++ src/components/manga/MangaToolbarMenu.tsx
@@ -9,7 +9,7 @@ import Label from '@mui/icons-material/Label';
 import MoreHoriz from '@mui/icons-material/MoreHoriz';
 import Refresh from '@mui/icons-material/Refresh';
 import {
-    IconButton, ListItemIcon, ListItemText, Menu, MenuItem,
+    IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Tooltip, useMediaQuery, useTheme,
 } from '@mui/material';
 import CategorySelect from 'components/navbar/action/CategorySelect';
 import React, { useState } from 'react';
@@ -21,6 +21,9 @@ interface IProps {
 }
 
 const MangaToolbarMenu = ({ manga, onRefresh, refreshing }: IProps) => {
+    const theme = useTheme();
+    const isLargeScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClose = () => {
@@ -31,47 +34,68 @@ const MangaToolbarMenu = ({ manga, onRefresh, refreshing }: IProps) => {
 
     return (
         <>
-            <IconButton
-                id="chaptersMenuButton"
-                aria-controls={open ? 'chaptersMenu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={(e) => setAnchorEl(e.currentTarget)}
-            >
-                <MoreHoriz />
-            </IconButton>
-            <Menu
-                id="chaptersMenu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                    'aria-labelledby': 'chaptersMenuButton',
-                }}
-            >
-                <MenuItem
-                    onClick={() => { onRefresh(); handleClose(); }}
-                    disabled={refreshing}
-                >
-                    <ListItemIcon>
-                        <Refresh fontSize="small" />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Reload data from source
-                    </ListItemText>
-                </MenuItem>
-                <MenuItem
-                    disabled={manga.inLibrary !== true}
-                    onClick={() => { setEditCategories(true); handleClose(); }}
-                >
-                    <ListItemIcon>
-                        <Label fontSize="small" />
-                    </ListItemIcon>
-                    <ListItemText>
-                        Edit manga categories
-                    </ListItemText>
-                </MenuItem>
-            </Menu>
+            {isLargeScreen && (
+                <>
+                    <Tooltip title="Reload data from source">
+                        <IconButton onClick={() => { onRefresh(); }} disabled={refreshing}>
+                            <Refresh />
+                        </IconButton>
+                    </Tooltip>
+                    {manga.inLibrary && (
+                        <Tooltip title="Edit manga categories">
+                            <IconButton onClick={() => { setEditCategories(true); }}>
+                                <Label />
+                            </IconButton>
+                        </Tooltip>
+                    )}
+                </>
+            )}
+            {!isLargeScreen && (
+                <>
+                    <IconButton
+                        id="chaptersMenuButton"
+                        aria-controls={open ? 'chaptersMenu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={(e) => setAnchorEl(e.currentTarget)}
+                    >
+                        <MoreHoriz />
+                    </IconButton>
+                    <Menu
+                        id="chaptersMenu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'chaptersMenuButton',
+                        }}
+                    >
+                        <MenuItem
+                            onClick={() => { onRefresh(); handleClose(); }}
+                            disabled={refreshing}
+                        >
+                            <ListItemIcon>
+                                <Refresh fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Reload data from source
+                            </ListItemText>
+                        </MenuItem>
+                        {manga.inLibrary && (
+                            <MenuItem
+                                onClick={() => { setEditCategories(true); handleClose(); }}
+                            >
+                                <ListItemIcon>
+                                    <Label fontSize="small" />
+                                </ListItemIcon>
+                                <ListItemText>
+                                    Edit manga categories
+                                </ListItemText>
+                            </MenuItem>
+                        )}
+                    </Menu>
+                </>
+            )}
 
             <CategorySelect
                 open={editCategories}
