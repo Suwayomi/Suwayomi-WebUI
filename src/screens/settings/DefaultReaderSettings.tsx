599bd768c38f6662e22cd910506569498f3a9b78
schroda <github@dasch.simplelogin.com>
2022-12-31 15:15:50 +0100

    [#214] Add component for changing the default reader settings

---
+++ src/screens/settings/DefaultReaderSettings.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable react/destructuring-assignment */
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import React, { useContext, useEffect } from 'react';
+import NavbarContext from 'components/context/NavbarContext';
+import { useQuery } from 'util/client';
+import { Box } from '@mui/system';
+import CircularProgress from '@mui/material/CircularProgress';
+import { requestUpdateServerMetadata } from 'util/metadata';
+import makeToast from 'components/util/Toast';
+import { getReaderSettingsFor } from 'util/readerSettings';
+import ReaderSettingsOptions from 'components/reader/ReaderSettingsOptions';
+
+export default function DefaultReaderSettings() {
+    const { setTitle, setAction } = useContext(NavbarContext);
+    useEffect(() => {
+        setTitle('Default Reader Settings');
+        setAction(<></>);
+    }, []);
+
+    const { data: meta, loading } = useQuery<IMetadata>('/api/v1/meta');
+    const settings = getReaderSettingsFor({ meta });
+
+    const setSettingValue = (key: keyof IReaderSettings, value: string | boolean) => {
+        requestUpdateServerMetadata(meta ?? {}, [[key, value]]).catch(() => makeToast('Failed to save the default reader settings to the server', 'warning'));
+    };
+
+    if (loading) {
+        return (
+            <Box sx={{
+                height: '100vh', width: '100vw', display: 'grid', placeItems: 'center',
+            }}
+            >
+                <CircularProgress thickness={5} />
+            </Box>
+        );
+    }
+
+    return (
+        <ReaderSettingsOptions
+            setSettingValue={setSettingValue}
+            staticNav={settings.staticNav}
+            showPageNumber={settings.showPageNumber}
+            loadNextonEnding={settings.loadNextonEnding}
+            readerType={settings.readerType}
+        />
+    );
+}
