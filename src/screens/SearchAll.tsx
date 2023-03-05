a7751fd3ab6e0a4ebd9e311789ad65eeed7f161f
schroda <50052685+schroda@users.noreply.github.com>
2023-03-05 14:31:21 +0100

    Upgrade "react-scripts" to v5 - Fix import

--- src/screens/SearchAll.tsx
+++ src/screens/SearchAll.tsx
@@ -11,7 +11,7 @@ import NavbarContext from 'components/context/NavbarContext';
 import MangaGrid from 'components/MangaGrid';
 import LangSelect from 'components/navbar/action/LangSelect';
 import AppbarSearch from 'components/util/AppbarSearch';
-import PQueue from 'p-queue/dist/index';
+import PQueue from 'p-queue';
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { StringParam, useQueryParam } from 'use-query-params';
