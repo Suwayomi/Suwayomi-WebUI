14ac872876922128de3e141d1f3c05b5d11da7ad
schroda <50052685+schroda@users.noreply.github.com>
2023-05-15 18:36:16 +0200

    Prevent manga page FAB from changing position when selecting chapters (#300)
    When selecting chapters the FAB button slightly moved further up and to the left.
    This happened because "em" was used for the positioning and the "SelectionFAB" being inside a "Box" component while the "Start/ResumeFAB" had "no extra" parent component.

--- src/components/util/StyledFab.tsx
+++ src/components/util/StyledFab.tsx
@@ -4,8 +4,8 @@ import { styled } from '@mui/system';
 export const DEFAULT_FAB_STYLE = {
     position: 'fixed',
     height: '48px',
-    right: '3em',
-    bottom: '2em',
+    right: '48px',
+    bottom: '28px',
 } as const;
 
 export const DEFAULT_FULL_FAB_HEIGHT = `calc(${DEFAULT_FAB_STYLE.bottom} + ${DEFAULT_FAB_STYLE.height})`;
