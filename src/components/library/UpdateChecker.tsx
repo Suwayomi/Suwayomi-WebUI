de90a7291cdfe23da16fb21bbb42adc06f37ce51
schroda <50052685+schroda@users.noreply.github.com>
2023-05-18 13:09:06 +0200

    [ESLint::eslint-plugin-header] Fix errors

--- src/components/library/UpdateChecker.tsx
+++ src/components/library/UpdateChecker.tsx
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import React, { useEffect, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import RefreshIcon from '@mui/icons-material/Refresh';
