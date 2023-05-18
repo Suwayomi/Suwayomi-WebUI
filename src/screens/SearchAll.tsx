Suwayomi/Suwayomi-WebUI

commit a457d80c446960df5644513fdf4d3a11974243c7

Feature/streamline backend requests (#297)
* Introduce "RequestManager"

* Use "RequestManager" - Simple replacements

 - Get rid of all "util/client" imports
 - replace old requests with new "RequestManager"
 - remove "fetcher" from global SWR config
     instead of using the SWR hooks by themselves, the "requestManager" is supposed to be used

* Use "RequestManager" - Do requests via SWR hooks

Use SWR hooks at places where it's easily usable

* Prevent trailing slashes in the "baseUrl"

src/screens/SearchAll.tsx
@@ -12,15 +12,15 @@ import MangaGrid from 'components/MangaGrid';
 import LangSelect from 'components/navbar/action/LangSelect';
 import AppbarSearch from 'components/util/AppbarSearch';
 import PQueue from 'p-queue';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { StringParam, useQueryParam } from 'use-query-params';
-import client from 'util/client';
 import { langSortCmp, sourceDefualtLangs, sourceForcedDefaultLangs } from 'util/language';
 import useLocalStorage from 'util/useLocalStorage';
 import { ISource } from 'typings';
 import { useTranslation } from 'react-i18next';
 import { translateExtensionLanguage } from 'screens/util/Extensions';
+import requestManager from 'lib/RequestManager';
 
 function sourceToLangList(sources: ISource[]) {
     const result: string[] = [];
@@ -46,9 +46,22 @@ const SearchAll: React.FC = () => {
     const [shownLangs, setShownLangs] = useLocalStorage<string[]>('shownSourceLangs', sourceDefualtLangs());
     const [showNsfw] = useLocalStorage<boolean>('showNsfw', true);
 
-    const [sources, setSources] = useState<ISource[]>([]);
+    const { data: unsortedSources = [], isLoading: FetchedSources } = requestManager.useGetSourceList();
+    const sources = useMemo(
+        () =>
+            unsortedSources.sort((a: { displayName: string }, b: { displayName: string }) => {
+                if (a.displayName < b.displayName) {
+                    return -1;
+                }
+                if (a.displayName > b.displayName) {
+                    return 1;
+                }
+                return 0;
+            }),
+        [unsortedSources],
+    );
+
     const [fetched, setFetched] = useState<any>({});
-    const [FetchedSources, setFetchedSources] = useState<any>({});
 
     const [lastPageNum, setLastPageNum] = useState<number>(1);
 
@@ -61,32 +74,12 @@ const SearchAll: React.FC = () => {
         setAction(<AppbarSearch />);
     }, [t]);
 
-    useEffect(() => {
-        client
-            .get('/api/v1/source/list')
-            .then((response) => response.data)
-            .then((data) => {
-                setSources(
-                    data.sort((a: { displayName: string }, b: { displayName: string }) => {
-                        if (a.displayName < b.displayName) {
-                            return -1;
-                        }
-                        if (a.displayName > b.displayName) {
-                            return 1;
-                        }
-                        return 0;
-                    }),
-                );
-                setFetchedSources(true);
-            });
-    }, []);
-
     async function doIT(elem: any[]) {
         elem.map((ele) =>
             limit.add(async () => {
-                const response = await client.get(
-                    `/api/v1/source/${ele.id}/search?searchTerm=${query || ''}&pageNum=1`,
-                );
+                const response = await requestManager
+                    .getClient()
+                    .get(`/api/v1/source/${ele.id}/search?searchTerm=${query || ''}&pageNum=1`);
                 const data = await response.data;
                 const tmp = mangas;
                 tmp[ele.id] = data.mangaList;

src/components/ExtensionCard.tsx
@@ -12,11 +12,10 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import client from 'util/client';
-import useLocalStorage from 'util/useLocalStorage';
 import { Box } from '@mui/system';
 import { IExtension, TranslationKey } from 'typings';
 import { useTranslation } from 'react-i18next';
+import requestManager from 'lib/RequestManager';
 
 interface IProps {
     extension: IExtension;
@@ -79,9 +78,6 @@ export default function ExtensionCard(props: IProps) {
         return installed ? InstalledState.UNINSTALL : InstalledState.INSTALL;
     });
 
-    const [serverAddress] = useLocalStorage<String>('serverBaseURL', '');
-    const [useCache] = useLocalStorage<boolean>('useCache', true);
-
     const langPress = lang === 'all' ? t('extension.language.all') : lang.toUpperCase();
 
     const requestExtensionAction = async (action: ExtensionAction): Promise<void> => {
@@ -89,7 +85,19 @@ export default function ExtensionCard(props: IProps) {
         const state = EXTENSION_ACTION_TO_STATE_MAP[action];
 
         setInstalledState(state);
-        await client.get(`/api/v1/extension/${action.toLowerCase()}/${pkgName}`);
+        switch (action) {
+            case ExtensionAction.INSTALL:
+                await requestManager.installExtension(pkgName);
+                break;
+            case ExtensionAction.UNINSTALL:
+                await requestManager.uninstallExtension(pkgName);
+                break;
+            case ExtensionAction.UPDATE:
+                await requestManager.updateExtension(pkgName);
+                break;
+            default:
+                throw new Error(`Unexpected ExtensionAction "${action}"`);
+        }
         setInstalledState(nextAction);
         notifyInstall();
     };
@@ -129,7 +137,7 @@ export default function ExtensionCard(props: IProps) {
                             mr: 2,
                         }}
                         alt={name}
-                        src={`${serverAddress}${iconUrl}?useCache=${useCache}`}
+                        src={requestManager.getValidImgUrlFor(iconUrl)}
                     />
                     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                         <Typography variant="h5" component="h2">

src/components/MangaCard.tsx
@@ -19,6 +19,7 @@ import { GridLayout, useLibraryOptionsContext } from 'components/context/Library
 import { BACK } from 'util/useBackTo';
 import { IMangaCard } from 'typings';
 import { useTranslation } from 'react-i18next';
+import requestManager from 'lib/RequestManager';
 
 const BottomGradient = styled('div')({
     position: 'absolute',
@@ -87,8 +88,6 @@ const MangaCard = React.forwardRef<HTMLDivElement, IProps>((props: IProps, ref) 
         options: { showUnreadBadge, showDownloadBadge },
     } = useLibraryOptionsContext();
 
-    const [serverAddress] = useLocalStorage<String>('serverBaseURL', '');
-    const [useCache] = useLocalStorage<boolean>('useCache', true);
     const [ItemWidth] = useLocalStorage<number>('ItemWidth', 300);
 
     const mangaLinkTo = { pathname: `/manga/${id}/`, state: { backLink: BACK } };
@@ -145,7 +144,7 @@ const MangaCard = React.forwardRef<HTMLDivElement, IProps>((props: IProps, ref) 
                                 </BadgeContainer>
                                 <SpinnerImage
                                     alt={title}
-                                    src={`${serverAddress}${thumbnailUrl}?useCache=${useCache}`}
+                                    src={requestManager.getValidImgUrlFor(thumbnailUrl)}
                                     imgStyle={
                                         inLibraryIndicator && inLibrary
                                             ? {
@@ -232,7 +231,7 @@ const MangaCard = React.forwardRef<HTMLDivElement, IProps>((props: IProps, ref) 
                                           imageRendering: 'pixelated',
                                       }
                             }
-                            src={`${serverAddress}${thumbnailUrl}?useCache=${useCache}`}
+                            src={requestManager.getValidImgUrlFor(thumbnailUrl)}
                         />
                         <Box
                             sx={{

src/components/SourceCard.tsx
@@ -16,9 +16,9 @@ import { Box, styled } from '@mui/system';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useHistory } from 'react-router-dom';
-import useLocalStorage from 'util/useLocalStorage';
 import { ISource } from 'typings';
 import { translateExtensionLanguage } from 'screens/util/Extensions';
+import requestManager from 'lib/RequestManager';
 
 const MobileWidthButtons = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -50,9 +50,6 @@ const SourceCard: React.FC<IProps> = (props: IProps) => {
     } = props;
 
     const history = useHistory();
-
-    const [serverAddress] = useLocalStorage<String>('serverBaseURL', '');
-    const [useCache] = useLocalStorage<boolean>('useCache', true);
 
     const redirectTo = (e: any, to: string) => {
         history.push(to);
@@ -86,7 +83,7 @@ const SourceCard: React.FC<IProps> = (props: IProps) => {
                                 flex: '0 0 auto',
                                 mr: 2,
                             }}
-                            src={`${serverAddress}${iconUrl}?useCache=${useCache}`}
+                            src={requestManager.getValidImgUrlFor(iconUrl)}
                         />
                         <Box
                             sx={{

src/components/context/AppContext.tsx
@@ -14,7 +14,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 import createTheme from 'theme';
 import { QueryParamProvider } from 'use-query-params';
-import { fetcher } from 'util/client';
 import useLocalStorage from 'util/useLocalStorage';
 import DarkTheme from 'components/context/DarkTheme';
 
@@ -36,7 +35,7 @@ const AppContext: React.FC<Props> = ({ children }) => {
     const theme = useMemo(() => createTheme(darkTheme), [darkTheme]);
 
     return (
-        <SWRConfig value={{ fetcher }}>
+        <SWRConfig>
             <Router>
                 <StyledEngineProvider injectFirst>
                     <ThemeProvider theme={theme}>

src/components/library/UpdateChecker.tsx
@@ -12,10 +12,10 @@ import RefreshIcon from '@mui/icons-material/Refresh';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Box } from '@mui/system';
 import Typography from '@mui/material/Typography';
-import client from 'util/client';
 import makeToast from 'components/util/Toast';
 import { IUpdateStatus } from 'typings';
 import { useTranslation } from 'react-i18next';
+import requestManager from 'lib/RequestManager';
 
 interface IProgressProps {
     progress: number;
@@ -32,8 +32,6 @@ function Progress({ progress }: IProgressProps) {
     );
 }
 
-const baseWebsocketUrl = JSON.parse(window.localStorage.getItem('serverBaseURL')!).replace('http', 'ws');
-
 interface IUpdateCheckerProps {
     handleFinishedUpdate: (time: number) => void;
 }
@@ -48,7 +46,7 @@ function UpdateChecker({ handleFinishedUpdate }: IUpdateCheckerProps) {
         try {
             setLoading(true);
             setProgress(0);
-            await client.post('/api/v1/update/fetch');
+            await requestManager.startGlobalUpdate();
         } catch (e) {
             makeToast(t('global.error.label.update_failed'), 'error');
             setLoading(false);
@@ -56,7 +54,7 @@ function UpdateChecker({ handleFinishedUpdate }: IUpdateCheckerProps) {
     };
 
     useEffect(() => {
-        const wsc = new WebSocket(`${baseWebsocketUrl}/api/v1/update`);
+        const wsc = requestManager.getUpdateWebSocket();
 
         // "loading" can't be used since it will be outdated once the state gets changed
         // it could be used by adding it as a dependency of "useEffect" but then the socket would

src/components/library/useSubscription.ts
@@ -7,14 +7,13 @@
  */
 
 import { useEffect, useState } from 'react';
-
-const baseWebsocketUrl = JSON.parse(window.localStorage.getItem('serverBaseURL')!).replace('http', 'ws');
+import requestManager from 'lib/RequestManager';
 
 const useSubscription = <T>(path: string, callback?: (newValue: T) => boolean | void) => {
     const [state, setState] = useState<T | undefined>();
 
     useEffect(() => {
-        const wsc = new WebSocket(`${baseWebsocketUrl}${path}`);
+        const wsc = new WebSocket(requestManager.getValidWebSocketUrl(path));
 
         wsc.onmessage = (e) => {
             const data = JSON.parse(e.data) as T;

src/components/manga/ChapterCard.tsx
@@ -27,11 +27,11 @@ import Typography from '@mui/material/Typography';
 import DownloadStateIndicator from 'components/molecules/DownloadStateIndicator';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import client from 'util/client';
 import { BACK } from 'util/useBackTo';
 import { getUploadDateString } from 'util/date';
 import { IChapter, IDownloadChapter } from 'typings';
 import { useTranslation } from 'react-i18next';
+import requestManager from 'lib/RequestManager';
 
 interface IProps {
     chapter: IChapter;
@@ -66,23 +66,23 @@ const ChapterCard: React.FC<IProps> = (props: IProps) => {
     const sendChange = (key: string, value: any) => {
         handleClose();
 
-        const formData = new FormData();
-        formData.append(key, value);
-        if (key === 'read') {
-            formData.append('lastPageRead', '0');
-        }
-        client
-            .patch(`/api/v1/manga/${chapter.mangaId}/chapter/${chapter.index}`, formData)
+        requestManager
+            .updateChapter(chapter.mangaId, chapter.index, {
+                [key]: value,
+                lastPageRead: key === 'read' ? 0 : undefined,
+            })
             .then(() => triggerChaptersUpdate());
     };
 
     const downloadChapter = () => {
-        client.get(`/api/v1/download/${chapter.mangaId}/chapter/${chapter.index}`);
+        requestManager.addChapterToDownloadQueue(chapter.mangaId, chapter.index);
         handleClose();
     };
 
     const deleteChapter = () => {
-        client.delete(`/api/v1/manga/${chapter.mangaId}/chapter/${chapter.index}`).then(() => triggerChaptersUpdate());
+        requestManager
+            .removeChapterFromDownloadQueue(chapter.mangaId, chapter.index)
+            .then(() => triggerChaptersUpdate());
         handleClose();
     };
 

commit 20fa06404169bf6a88c5b1e50d0afb16e534bad0

Trigger global search request
Issue introduced with a457d80c446960df5644513fdf4d3a11974243c7.
"FetchedSources" was changed to represent the loading state of the request, but it was forgotten to update its usage

src/screens/SearchAll.tsx
@@ -46,7 +46,7 @@ const SearchAll: React.FC = () => {
     const [shownLangs, setShownLangs] = useLocalStorage<string[]>('shownSourceLangs', sourceDefualtLangs());
     const [showNsfw] = useLocalStorage<boolean>('showNsfw', true);
 
-    const { data: unsortedSources = [], isLoading: FetchedSources } = requestManager.useGetSourceList();
+    const { data: unsortedSources = [], isLoading: isLoadingSources } = requestManager.useGetSourceList();
     const sources = useMemo(
         () =>
             unsortedSources.sort((a: { displayName: string }, b: { displayName: string }) => {
@@ -116,7 +116,7 @@ const SearchAll: React.FC = () => {
     }, [ResetUI]);
 
     useEffect(() => {
-        if (query && FetchedSources) {
+        if (query && !isLoadingSources) {
             const delayDebounceFn = setTimeout(() => {
                 setTriggerUpdate(0);
             }, 1000);