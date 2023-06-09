fa894a6e9cb6dab6c4d9ce181b321e3c1997c1f5
schroda <50052685+schroda@users.noreply.github.com>
2023-06-09 20:52:54 +0200

    Add info about dev mode + strict mode issue

--- src/screens/SourceMangas.tsx
+++ src/screens/SourceMangas.tsx
@@ -248,6 +248,9 @@ export default function SourceMangas() {
                 return;
             }
 
+            // INFO:
+            // with strict mode + dev mode the first request will be aborted. due to using SWR there won't be an
+            // immediate second request since it's the same key. instead the "second" request will be the error handling of SWR
             abortRequest(new Error(`SourceMangas(${sourceId}): search string changed`));
         },
         [searchTerm, contentType],
