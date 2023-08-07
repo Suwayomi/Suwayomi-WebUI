fcd7b4483e8545fbcaaa701db9df693e3f92c3a8
schroda <50052685+schroda@users.noreply.github.com>
2023-08-07 21:49:04 +0200

    Introduce script to create a changelog

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,49 +4,61 @@ module.exports = {
     parserOptions: {
         project: './tsconfig.json',
     },
-    rules: {
-        'header/header': [
-            'error',
-            'block', // comment type
-            [ // expected file header comment
-                // the spaces are important, otherwise, the rule will mark it as an error
-                '',
-                ' * Copyright (C) Contributors to the Suwayomi project',
-                ' *',
-                ' * This Source Code Form is subject to the terms of the Mozilla Public',
-                ' * License, v. 2.0. If a copy of the MPL was not distributed with this',
-                ' * file, You can obtain one at https://mozilla.org/MPL/2.0/.',
-                ' ',
-            ],
-            2, // number of new lines after comment
-        ],
+    overrides: [
+        {
+            files: ['*'],
+            rules: {
+                'header/header': [
+                    'error',
+                    'block', // comment type
+                    [
+                        // expected file header comment
+                        // the spaces are important, otherwise, the rule will mark it as an error
+                        '',
+                        ' * Copyright (C) Contributors to the Suwayomi project',
+                        ' *',
+                        ' * This Source Code Form is subject to the terms of the Mozilla Public',
+                        ' * License, v. 2.0. If a copy of the MPL was not distributed with this',
+                        ' * file, You can obtain one at https://mozilla.org/MPL/2.0/.',
+                        ' ',
+                    ],
+                    2, // number of new lines after comment
+                ],
 
-        'prettier/prettier': 'error',
+                'prettier/prettier': 'error',
 
-        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+                'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
 
-        // just why
-        'react/jsx-no-bind': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/require-default-props': 'off',
-        'react/function-component-definition': 'off',
+                // just why
+                'react/jsx-no-bind': 'off',
+                'react/jsx-props-no-spreading': 'off',
+                'react/require-default-props': 'off',
+                'react/function-component-definition': 'off',
 
-        'react/no-unstable-nested-components': [
-            'error',
-            {
-                allowAsProps: true,
+                'react/no-unstable-nested-components': [
+                    'error',
+                    {
+                        allowAsProps: true,
+                    },
+                ],
+
+                // seems to be bugged for aliases
+                'import/extensions': ['error', 'ignorePackages', { '': 'never' }],
+
+                'no-relative-import-paths/no-relative-import-paths': [
+                    'error',
+                    {
+                        rootDir: 'src',
+                        prefix: '@',
+                    },
+                ],
             },
-        ],
-
-        // seems to be bugged for aliases
-        'import/extensions': ['error', 'ignorePackages', { '': 'never' }],
-
-        'no-relative-import-paths/no-relative-import-paths': [
-            'error',
-            {
-                rootDir: 'src',
-                prefix: '@',
+        },
+        {
+            files: ['tools/script/.*'],
+            rules: {
+                'no-relative-import-paths/no-relative-import-paths': 'off'
             },
-        ],
-    },
+        },
+    ],
 };
