Suwayomi/Suwayomi-WebUI

commit 446deeae06b4c8f5f189685969105b185043f0a7

Feature/library manga actions (#506)
* Extend "IMangaGridProps" from "DefaultGridProps"

* Move library manga filtering into hook

* Add logic to select mangas

* Add manga actions

* Remove cache only policy for category mangas

Unclear why this was added

* Prevent SelectionFAB from being hidden by the mobile footer

On e.g. the library page, the footer is visible and thus, the fab was not completely visible

* Update categories of manga only after clicking "OK"

Previously, selecting a category resulted in an immediate mutation.
To be able to reuse the "CategorySelect" component to change the categories of multiple mangas at once, this behaviour is not suited.

* Add action to change categories of multiple mangas

* Cancel selection mode after removal from library

The mangas would still be selected and other action could get performed. However, this should only be possible for mangas that are in the library

* Remove unintentionally added console log

Accidentally added with 980da657d99dd8d01fed82b5c80be7e3b01d0429

src/components/navbar/action/CategorySelect.tsx
@@ -6,33 +6,80 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import Button from '@mui/material/Button';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Dialog from '@mui/material/Dialog';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import { useTranslation } from 'react-i18next';
 import { requestManager } from '@/lib/requests/RequestManager.ts';
+import { Mangas } from '@/lib/data/Mangas.ts';
+import { useSelectableCollection } from '@/components/collection/useSelectableCollection.ts';
+import { ThreeStateCheckboxInput } from '@/components/atoms/ThreeStateCheckboxInput.tsx';
 
-interface IProps {
+type BaseProps = {
     open: boolean;
     setOpen: (value: boolean) => void;
-    mangaId: number;
-}
+};
 
-export function CategorySelect(props: IProps) {
+type SingleMangaModeProps = {
+    mangaId: number;
+};
+
+type MultiMangaModeProps = {
+    mangaIds: number[];
+};
+
+type Props =
+    | (BaseProps & SingleMangaModeProps & PropertiesNever<MultiMangaModeProps>)
+    | (BaseProps & PropertiesNever<SingleMangaModeProps> & MultiMangaModeProps);
+
+const useGetMangaCategoryIds = (mangaId: number | undefined): number[] => {
+    const { data: mangaResult } = requestManager.useGetManga(mangaId ?? -1, { skip: mangaId === undefined });
+
+    return useMemo(() => {
+        if (mangaId === undefined || !mangaResult) {
+            return [];
+        }
+
+        return mangaResult.manga.categories.nodes.map((category) => category.id);
+    }, [mangaResult?.manga.categories.nodes, mangaId]);
+};
+
+const getCategoryCheckedState = (
+    categoryId: number,
+    categoriesToAdd: number[],
+    categoriesToRemove: number[],
+    isSingleSelectionMode: boolean,
+): boolean | undefined => {
+    if (categoriesToAdd.includes(categoryId)) {
+        return true;
+    }
+
+    if (isSingleSelectionMode) {
+        return undefined;
+    }
+
+    if (categoriesToRemove.includes(categoryId)) {
+        return false;
+    }
+
+    return undefined;
+};
+
+export function CategorySelect(props: Props) {
     const { t } = useTranslation();
 
-    const { open, setOpen, mangaId } = props;
+    const { open, setOpen, mangaId, mangaIds: passedMangaIds } = props;
 
-    const { data: mangaResult } = requestManager.useGetManga(mangaId);
+    const isSingleSelectionMode = mangaId !== undefined;
+    const mangaIds = passedMangaIds ?? [mangaId];
+
+    const mangaCategories = useGetMangaCategoryIds(mangaId);
     const { data } = requestManager.useGetCategories();
     const categoriesData = data?.categories.nodes;
-    const [triggerMutate] = requestManager.useUpdateMangaCategories();
 
     const allCategories = useMemo(() => {
         const cats = [...(categoriesData ?? [])]; // make copy
@@ -42,29 +89,45 @@ export function CategorySelect(props: IProps) {
         return cats;
     }, [categoriesData]);
 
-    const selectedIds = mangaResult?.manga.categories.nodes.map((c) => c.id) ?? [];
+    const { handleSelection, setSelectionForKey, getSelectionForKey } = useSelectableCollection<
+        number,
+        'categoriesToAdd' | 'categoriesToRemove'
+    >(allCategories.length, {
+        currentKey: 'categoriesToAdd',
+        initialState: {
+            categoriesToAdd: mangaCategories,
+            categoriesToRemove: [],
+        },
+    });
+
+    const categoriesToAdd = getSelectionForKey('categoriesToAdd');
+    const categoriesToRemove = getSelectionForKey('categoriesToRemove');
 
     const handleCancel = () => {
+        setSelectionForKey('categoriesToAdd', mangaCategories);
+        setSelectionForKey('categoriesToRemove', []);
         setOpen(false);
     };
 
     const handleOk = () => {
         setOpen(false);
-    };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, categoryId: number) => {
-        const { checked } = event.target as HTMLInputElement;
+        const addToCategories = isSingleSelectionMode
+            ? categoriesToAdd.filter((categoryId) => !mangaCategories.includes(categoryId))
+            : categoriesToAdd;
+        const removeFromCategories = isSingleSelectionMode
+            ? mangaCategories.filter((categoryId) => !categoriesToAdd.includes(categoryId))
+            : categoriesToRemove;
 
-        // TODO - update to only update categories when clicking OK - can now be updated in one go with graphql
-        triggerMutate({
-            variables: {
-                input: {
-                    id: mangaId,
-                    patch: {
-                        addToCategories: checked ? [categoryId] : [],
-                        removeFromCategories: !checked ? [categoryId] : [],
-                    },
-                },
+        const isUpdateRequired = !!addToCategories.length || !!removeFromCategories.length;
+        if (!isUpdateRequired) {
+            return;
+        }
+
+        Mangas.performAction('change_categories', mangaIds, {
+            changeCategoriesPatch: {
+                addToCategories,
+                removeFromCategories,
             },
         });
     };
@@ -91,14 +154,25 @@ export function CategorySelect(props: IProps) {
                         </span>
                     )}
                     {allCategories.map((category) => (
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={selectedIds.includes(category.id)}
-                                    onChange={(e) => handleChange(e, category.id)}
-                                    color="default"
-                                />
-                            }
+                        <ThreeStateCheckboxInput
+                            checked={getCategoryCheckedState(
+                                category.id,
+                                categoriesToAdd,
+                                categoriesToRemove,
+                                isSingleSelectionMode,
+                            )}
+                            onChange={(checked) => {
+                                handleSelection(category.id, false, 'categoriesToAdd');
+                                handleSelection(category.id, false, 'categoriesToRemove');
+
+                                if (checked) {
+                                    handleSelection(category.id, true, 'categoriesToAdd');
+                                }
+
+                                if (checked === false) {
+                                    handleSelection(category.id, true, 'categoriesToRemove');
+                                }
+                            }}
                             label={category.name}
                             key={category.id}
                         />

src/components/MangaGrid.tsx
@@ -10,13 +10,15 @@ import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 're
 import Grid, { GridTypeMap } from '@mui/material/Grid';
 import { Box, Typography } from '@mui/material';
 import { GridItemProps, GridStateSnapshot, VirtuosoGrid } from 'react-virtuoso';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { EmptyView } from '@/components/util/EmptyView';
 import { LoadingPlaceholder } from '@/components/util/LoadingPlaceholder';
 import { MangaCard } from '@/components/MangaCard';
 import { GridLayout } from '@/components/context/LibraryOptionsContext';
 import { useLocalStorage } from '@/util/useLocalStorage';
-import { TPartialManga } from '@/typings.ts';
+import { TManga, TPartialManga } from '@/typings.ts';
+import { SelectableCollectionReturnType } from '@/components/collection/useSelectableCollection.ts';
+import { DEFAULT_FULL_FAB_HEIGHT } from '@/components/util/StyledFab.tsx';
 
 const GridContainer = React.forwardRef<HTMLDivElement, GridTypeMap['props']>(({ children, ...props }, ref) => (
     <Grid {...props} ref={ref} container sx={{ paddingLeft: '5px', paddingRight: '13px' }}>
@@ -40,8 +42,22 @@ const GridItemContainerWithDimension = (
     );
 };
 
-const createMangaCard = (manga: TPartialManga, gridLayout?: GridLayout, inLibraryIndicator?: boolean) => (
-    <MangaCard key={manga.id} manga={manga} gridLayout={gridLayout} inLibraryIndicator={inLibraryIndicator} />
+const createMangaCard = (
+    manga: TPartialManga,
+    gridLayout?: GridLayout,
+    inLibraryIndicator?: boolean,
+    isSelectModeActive: boolean = false,
+    selectedMangaIds?: TManga['id'][],
+    handleSelection?: DefaultGridProps['handleSelection'],
+) => (
+    <MangaCard
+        key={manga.id}
+        manga={manga}
+        gridLayout={gridLayout}
+        inLibraryIndicator={inLibraryIndicator}
+        selected={isSelectModeActive ? selectedMangaIds?.includes(manga.id) : null}
+        handleSelection={handleSelection}
+    />
 );
 
 type DefaultGridProps = {
@@ -50,9 +66,21 @@ type DefaultGridProps = {
     inLibraryIndicator?: boolean;
     GridItemContainer: (props: GridTypeMap['props'] & Partial<GridItemProps>) => JSX.Element;
     gridLayout?: GridLayout;
+    isSelectModeActive?: boolean;
+    selectedMangaIds?: Required<TManga['id']>[];
+    handleSelection?: SelectableCollectionReturnType<TManga['id']>['handleSelection'];
 };
 
-const HorizontalGrid = ({ isLoading, mangas, inLibraryIndicator, GridItemContainer, gridLayout }: DefaultGridProps) => (
+const HorizontalGrid = ({
+    isLoading,
+    mangas,
+    inLibraryIndicator,
+    GridItemContainer,
+    gridLayout,
+    isSelectModeActive,
+    selectedMangaIds,
+    handleSelection,
+}: DefaultGridProps) => (
     <Grid
         container
         spacing={1}
@@ -70,7 +98,14 @@ const HorizontalGrid = ({ isLoading, mangas, inLibraryIndicator, GridItemContain
         ) : (
             mangas.map((manga) => (
                 <GridItemContainer key={manga.id}>
-                    {createMangaCard(manga, gridLayout, inLibraryIndicator)}
+                    {createMangaCard(
+                        manga,
+                        gridLayout,
+                        inLibraryIndicator,
+                        isSelectModeActive,
+                        selectedMangaIds,
+                        handleSelection,
+                    )}
                 </GridItemContainer>
             ))
         )}
@@ -85,6 +120,9 @@ const VerticalGrid = ({
     gridLayout,
     hasNextPage,
     loadMore,
+    isSelectModeActive,
+    selectedMangaIds,
+    handleSelection,
 }: DefaultGridProps & {
     hasNextPage: boolean;
     loadMore: () => void;
@@ -125,26 +163,38 @@ const VerticalGrid = ({
                 restoreStateFrom={snapshot}
                 stateChanged={persistGridState}
                 endReached={() => loadMore()}
-                itemContent={(index) => createMangaCard(mangas[index], gridLayout, inLibraryIndicator)}
+                itemContent={(index) =>
+                    createMangaCard(
+                        mangas[index],
+                        gridLayout,
+                        inLibraryIndicator,
+                        isSelectModeActive,
+                        selectedMangaIds,
+                        handleSelection,
+                    )
+                }
             />
             {/* render div to prevent UI jumping around when showing/hiding loading placeholder */
             /* eslint-disable-next-line no-nested-ternary */}
-            {isLoading ? <LoadingPlaceholder /> : hasNextPage ? <div style={{ height: '75px' }} /> : null}
+            {isSelectModeActive && gridLayout === GridLayout.List ? (
+                <Box sx={{ paddingBottom: DEFAULT_FULL_FAB_HEIGHT }} />
+            ) : // eslint-disable-next-line no-nested-ternary
+            isLoading ? (
+                <LoadingPlaceholder />
+            ) : hasNextPage ? (
+                <div style={{ height: '75px' }} />
+            ) : null}
         </>
     );
 };
 
-export interface IMangaGridProps {
-    mangas: TPartialManga[];
-    isLoading: boolean;
+export interface IMangaGridProps extends Omit<DefaultGridProps, 'GridItemContainer'> {
     message?: string;
     messageExtra?: JSX.Element;
     hasNextPage: boolean;
     loadMore: () => void;
-    gridLayout?: GridLayout;
     horizontal?: boolean | undefined;
     noFaces?: boolean | undefined;
-    inLibraryIndicator?: boolean;
 }
 
 export const MangaGrid: React.FC<IMangaGridProps> = (props) => {
@@ -159,6 +209,9 @@ export const MangaGrid: React.FC<IMangaGridProps> = (props) => {
         horizontal,
         noFaces,
         inLibraryIndicator,
+        isSelectModeActive,
+        selectedMangaIds,
+        handleSelection,
     } = props;
 
     const [dimensions, setDimensions] = useState(document.documentElement.offsetWidth);
@@ -221,6 +274,9 @@ export const MangaGrid: React.FC<IMangaGridProps> = (props) => {
                     inLibraryIndicator={inLibraryIndicator}
                     GridItemContainer={GridItemContainer}
                     gridLayout={gridLayout}
+                    isSelectModeActive={isSelectModeActive}
+                    selectedMangaIds={selectedMangaIds}
+                    handleSelection={handleSelection}
                 />
             ) : (
                 <VerticalGrid
@@ -231,6 +287,9 @@ export const MangaGrid: React.FC<IMangaGridProps> = (props) => {
                     hasNextPage={hasNextPage}
                     loadMore={loadMore}
                     gridLayout={gridLayout}
+                    isSelectModeActive={isSelectModeActive}
+                    selectedMangaIds={selectedMangaIds}
+                    handleSelection={handleSelection}
                 />
             )}
         </div>

src/components/collection/SelectableCollectionSelectMode.tsx
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Tooltip } from '@mui/material';
+import Checkbox from '@mui/material/Checkbox';
+import { useTranslation } from 'react-i18next';
+import ClearIcon from '@mui/icons-material/Clear';
+import { SelectableCollectionSelectAll } from '@/components/collection/SelectableCollectionSelectAll.tsx';
+
+export const SelectableCollectionSelectMode = ({
+    isActive,
+    areAllItemsSelected,
+    areNoItemsSelected,
+    onSelectAll,
+    onModeChange,
+}: {
+    isActive: boolean;
+    areAllItemsSelected: boolean;
+    areNoItemsSelected: boolean;
+    onSelectAll: (selectAll: boolean) => void;
+    onModeChange: (checked: boolean) => void;
+}) => {
+    const { t } = useTranslation();
+
+    return (
+        <>
+            {isActive && (
+                <SelectableCollectionSelectAll
+                    areAllItemsSelected={areAllItemsSelected}
+                    areNoItemsSelected={areNoItemsSelected}
+                    onChange={onSelectAll}
+                />
+            )}
+            <Tooltip title={t(!isActive ? 'global.button.select_all' : 'global.button.cancel')}>
+                <Checkbox
+                    checkedIcon={<ClearIcon />}
+                    sx={{ padding: '8px' }}
+                    checked={isActive}
+                    onChange={(_, checked) => onModeChange(checked)}
+                />
+            </Tooltip>
+        </>
+    );
+};

src/components/collection/useSelectableCollection.ts
@@ -8,34 +8,92 @@
 
 import { useState } from 'react';
 
-export const useSelectableCollection = <Id extends number | string>(totalCount: number) => {
-    const [selectedItemIds, setSelectedItemIds] = useState<Id[]>([]);
+export type SelectableCollectionReturnType<Id extends number | string, Key extends string = string> = {
+    selectedItemIds: Id[];
+    keySelectedItemIds: Id[];
+    areAllItemsSelected: boolean;
+    areNoItemsSelected: boolean;
+    areAllItemsForKeySelected: boolean;
+    areNoItemsForKeySelected: boolean;
+    handleSelection: (id: Id, selected: boolean, key?: Key) => void;
+    handleSelectAll: (selectAll: boolean, itemIds: Id[], key?: Key) => void;
+    setSelectionForKey: (key: Key, itemIds: Id[]) => void;
+    getSelectionForKey: (key: Key) => Id[];
+};
 
+export const useSelectableCollection = <Id extends number | string, Key extends string = 'default'>(
+    totalCount: number,
+    {
+        keyCount = totalCount,
+        currentKey,
+        initialState = {} as Record<Key, Id[]>,
+    }: {
+        keyCount?: number;
+        currentKey: Key;
+        initialState?: Record<Key, Id[]>;
+    },
+): SelectableCollectionReturnType<Id, Key> => {
+    const [keyToSelectedItemIds, setKeyToSelectedItemIds] = useState<Record<string, Id[]>>(initialState);
+
+    const selectedItemIds = Object.values(keyToSelectedItemIds).flat();
     const areAllItemsSelected = selectedItemIds.length === totalCount;
     const areNoItemsSelected = !selectedItemIds.length;
 
-    const handleSelection = (id: Id, selected: boolean) => {
+    const keySelectedItemIds = keyToSelectedItemIds[currentKey] ?? [];
+    const areAllItemsForKeySelected = keySelectedItemIds.length === keyCount;
+    const areNoItemsForKeySelected = keySelectedItemIds.length === 0;
+
+    const handleSelection = (id: Id, selected: boolean, key: Key = currentKey) => {
         const deselect = !selected;
         if (deselect) {
-            setSelectedItemIds(selectedItemIds.filter((selectedItemId) => selectedItemId !== id));
+            setKeyToSelectedItemIds((prevState) => ({
+                ...prevState,
+                [key]: prevState[key].filter((selectedItemId) => selectedItemId !== id),
+            }));
             return;
         }
 
-        setSelectedItemIds([...new Set([...selectedItemIds, id])]);
+        setKeyToSelectedItemIds((prevState) => ({
+            ...prevState,
+            [key]: [...new Set([...(prevState[key] ?? []), id])],
+        }));
     };
 
-    const handleSelectAll = (selectAll: boolean, itemIds: Id[]) => {
+    const handleSelectAll = (selectAll: boolean, itemIds: Id[], key: Key = currentKey) => {
         switch (selectAll) {
             case true:
-                setSelectedItemIds([...itemIds]);
+                setKeyToSelectedItemIds((prevState) => ({
+                    ...prevState,
+                    [key]: [...itemIds],
+                }));
                 break;
             case false:
-                setSelectedItemIds([]);
+                setKeyToSelectedItemIds((prevState) => ({
+                    ...prevState,
+                    [key]: [],
+                }));
                 break;
             default:
                 break;
         }
     };
 
-    return { selectedItemIds, handleSelection, handleSelectAll, areAllItemsSelected, areNoItemsSelected };
+    const setSelectionForKey = (key: Key, itemIds: Id[]) => {
+        keyToSelectedItemIds[key] = itemIds;
+    };
+
+    const getSelectionForKey = (key: Key) => keyToSelectedItemIds[key];
+
+    return {
+        selectedItemIds,
+        keySelectedItemIds,
+        handleSelection,
+        handleSelectAll,
+        areAllItemsSelected,
+        areNoItemsSelected,
+        areAllItemsForKeySelected,
+        areNoItemsForKeySelected,
+        setSelectionForKey,
+        getSelectionForKey,
+    };
 };

src/components/library/LibraryMangaGrid.tsx
@@ -6,128 +6,35 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import { StringParam, useQueryParam } from 'use-query-params';
 import { useTranslation } from 'react-i18next';
-import { LibrarySortMode, NullAndUndefined, TManga } from '@/typings';
-import { useSearchSettings } from '@/util/searchSettings';
+import { TManga } from '@/typings';
 import { useLibraryOptionsContext } from '@/components/context/LibraryOptionsContext';
-import { MangaGrid } from '@/components/MangaGrid';
+import { IMangaGridProps, MangaGrid } from '@/components/MangaGrid';
 
-const unreadFilter = (unread: NullAndUndefined<boolean>, { unreadCount }: TManga): boolean => {
-    switch (unread) {
-        case true:
-            return !!unreadCount && unreadCount >= 1;
-        case false:
-            return unreadCount === 0;
-        default:
-            return true;
-    }
-};
-
-const downloadedFilter = (downloaded: NullAndUndefined<boolean>, { downloadCount }: TManga): boolean => {
-    switch (downloaded) {
-        case true:
-            return !!downloadCount && downloadCount >= 1;
-        case false:
-            return downloadCount === 0;
-        default:
-            return true;
-    }
-};
-
-const queryFilter = (query: NullAndUndefined<string>, { title }: TManga): boolean => {
-    if (!query) return true;
-    return title.toLowerCase().includes(query.toLowerCase());
-};
-
-const queryGenreFilter = (query: NullAndUndefined<string>, { genre }: TManga): boolean => {
-    if (!query) return true;
-    const queries = query.split(',').map((str) => str.toLowerCase().trim());
-    return queries.every((element) => genre.map((el) => el.toLowerCase()).includes(element));
-};
-
-const filterManga = (
-    mangas: TManga[],
-    query: NullAndUndefined<string>,
-    unread: NullAndUndefined<boolean>,
-    downloaded: NullAndUndefined<boolean>,
-    ignoreFilters: boolean,
-): TManga[] =>
-    mangas.filter((manga) => {
-        const ignoreFiltersWhileSearching = ignoreFilters && query?.length;
-        const matchesSearch = queryFilter(query, manga) || queryGenreFilter(query, manga);
-        const matchesFilters =
-            ignoreFiltersWhileSearching || (downloadedFilter(downloaded, manga) && unreadFilter(unread, manga));
-
-        return matchesSearch && matchesFilters;
-    });
-
-const sortByUnread = (a: TManga, b: TManga): number => (a.unreadCount ?? 0) - (b.unreadCount ?? 0);
-
-const sortByTitle = (a: TManga, b: TManga): number => a.title.localeCompare(b.title);
-
-const sortByDateAdded = (a: TManga, b: TManga): number => Number(a.inLibraryAt) - Number(b.inLibraryAt);
-
-const sortByLastRead = (a: TManga, b: TManga): number =>
-    Number(b.lastReadChapter?.lastReadAt ?? 0) - Number(a.lastReadChapter?.lastReadAt ?? 0);
-
-const sortManga = (
-    manga: TManga[],
-    sort: NullAndUndefined<LibrarySortMode>,
-    desc: NullAndUndefined<boolean>,
-): TManga[] => {
-    const result = [...manga];
-
-    switch (sort) {
-        case 'sortAlph':
-            result.sort(sortByTitle);
-            break;
-        case 'sortDateAdded':
-            result.sort(sortByDateAdded);
-            break;
-        case 'sortToRead':
-            result.sort(sortByUnread);
-            break;
-        case 'sortLastRead':
-            result.sort(sortByLastRead);
-            break;
-        default:
-            break;
-    }
-
-    if (desc === true) {
-        result.reverse();
-    }
-
-    return result;
-};
-
-interface LibraryMangaGridProps {
+interface LibraryMangaGridProps
+    extends Required<Pick<IMangaGridProps, 'isSelectModeActive' | 'selectedMangaIds' | 'handleSelection'>> {
     mangas: TManga[];
+    showFilteredOutMessage: boolean;
     isLoading: boolean;
     message?: string;
 }
 
-export const LibraryMangaGrid: React.FC<LibraryMangaGridProps> = ({ mangas, isLoading, message }) => {
+export const LibraryMangaGrid: React.FC<LibraryMangaGridProps> = ({
+    mangas,
+    showFilteredOutMessage,
+    isLoading,
+    message,
+    isSelectModeActive,
+    selectedMangaIds,
+    handleSelection,
+}) => {
     const { t } = useTranslation();
 
     const [query] = useQueryParam('query', StringParam);
     const { options } = useLibraryOptionsContext();
     const { unread, downloaded } = options;
-    const { settings } = useSearchSettings();
-
-    const filteredMangas = useMemo(
-        () => filterManga(mangas, query, unread, downloaded, settings.ignoreFilters),
-        [mangas, query, unread, downloaded, settings.ignoreFilters],
-    );
-    const sortedMangas = useMemo(
-        () => sortManga(filteredMangas, options.sorts, options.sortDesc),
-        [filteredMangas, options.sorts, options.sortDesc],
-    );
-
-    const showFilteredOutMessage =
-        (unread != null || downloaded != null || query) && filteredMangas.length === 0 && mangas.length > 0;
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -135,12 +42,15 @@ export const LibraryMangaGrid: React.FC<LibraryMangaGridProps> = ({ mangas, isLo
 
     return (
         <MangaGrid
-            mangas={sortedMangas}
+            mangas={mangas}
             isLoading={isLoading}
             hasNextPage={false}
             loadMore={() => undefined}
             message={showFilteredOutMessage ? t('library.error.label.no_matches') : message}
             gridLayout={options.gridLayout}
+            isSelectModeActive={isSelectModeActive}
+            selectedMangaIds={selectedMangaIds}
+            handleSelection={handleSelection}
         />
     );
 };

commit 024a5b72a2b3eff88dbf2a9dfd943e282d9a0a2e

Update manga category selection in case categories changed
After a manga got added to the library, the category selection dialog did not show the categories the manga got automatically added to.
The selection only got updated after the dialog got closed and opened again

Regression was introduced with 446deeae06b4c8f5f189685969105b185043f0a7

src/components/navbar/action/CategorySelect.tsx
@@ -6,7 +6,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import Button from '@mui/material/Button';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
@@ -77,7 +77,7 @@ export function CategorySelect(props: Props) {
     const isSingleSelectionMode = mangaId !== undefined;
     const mangaIds = passedMangaIds ?? [mangaId];
 
-    const mangaCategories = useGetMangaCategoryIds(mangaId);
+    const mangaCategoryIds = useGetMangaCategoryIds(mangaId);
     const { data } = requestManager.useGetCategories();
     const categoriesData = data?.categories.nodes;
 
@@ -95,16 +95,21 @@ export function CategorySelect(props: Props) {
     >(allCategories.length, {
         currentKey: 'categoriesToAdd',
         initialState: {
-            categoriesToAdd: mangaCategories,
+            categoriesToAdd: mangaCategoryIds,
             categoriesToRemove: [],
         },
     });
+
+    useEffect(() => {
+        setSelectionForKey('categoriesToAdd', mangaCategoryIds);
+        setSelectionForKey('categoriesToRemove', []);
+    }, [mangaCategoryIds]);
 
     const categoriesToAdd = getSelectionForKey('categoriesToAdd');
     const categoriesToRemove = getSelectionForKey('categoriesToRemove');
 
     const handleCancel = () => {
-        setSelectionForKey('categoriesToAdd', mangaCategories);
+        setSelectionForKey('categoriesToAdd', mangaCategoryIds);
         setSelectionForKey('categoriesToRemove', []);
         setOpen(false);
     };
@@ -113,10 +118,10 @@ export function CategorySelect(props: Props) {
         setOpen(false);
 
         const addToCategories = isSingleSelectionMode
-            ? categoriesToAdd.filter((categoryId) => !mangaCategories.includes(categoryId))
+            ? categoriesToAdd.filter((categoryId) => !mangaCategoryIds.includes(categoryId))
             : categoriesToAdd;
         const removeFromCategories = isSingleSelectionMode
-            ? mangaCategories.filter((categoryId) => !categoriesToAdd.includes(categoryId))
+            ? mangaCategoryIds.filter((categoryId) => !categoriesToAdd.includes(categoryId))
             : categoriesToRemove;
 
         const isUpdateRequired = !!addToCategories.length || !!removeFromCategories.length;