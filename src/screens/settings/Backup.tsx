abdd5d1f7d5aeb5e68a83e7f829397daa1e34da7
schroda <50052685+schroda@users.noreply.github.com>
2024-01-25 02:19:35 +0100

    Replace static strings with translation keys

--- src/screens/settings/Backup.tsx
+++ src/screens/settings/Backup.tsx
@@ -281,7 +281,7 @@ export function Backup() {
                         minValue={1}
                         maxValue={31}
                         stepSize={1}
-                        valueUnit="Day"
+                        valueUnit={t('global.date.label.day')}
                         showSlider
                         handleUpdate={(interval: number) => updateSetting('backupInterval', interval)}
                     />
@@ -293,7 +293,7 @@ export function Backup() {
                         minValue={0}
                         maxValue={1000}
                         stepSize={1}
-                        valueUnit="Day"
+                        valueUnit={t('global.date.label.day')}
                         showSlider
                         handleUpdate={(ttl: number) => updateSetting('backupTTL', ttl)}
                     />
