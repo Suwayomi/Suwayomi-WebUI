Suwayomi/Suwayomi-WebUI

commit 506e0aa0e38c0bcb26931b85ef1d6d688b2d95ca

Update extension list after removing an obsolete extension (#579)
The removed extension was never removed from the cached list

src/lib/requests/RequestManager.ts
@@ -1015,7 +1015,7 @@ export class RequestManager {
 
     public updateExtension(
         id: string,
-        patch: UpdateExtensionPatchInput,
+        { isObsolete = false, ...patch }: UpdateExtensionPatchInput & { isObsolete?: boolean },
         options?: MutationOptions<UpdateExtensionMutation, UpdateExtensionMutationVariables>,
     ): AbortableApolloMutationResponse<UpdateExtensionMutation> {
         const result = this.doRequest<UpdateExtensionMutation, UpdateExtensionMutationVariables>(
@@ -1042,18 +1042,22 @@ export class RequestManager {
                     ...cachedExtensions.data,
                     fetchExtensions: {
                         ...cachedExtensions.data.fetchExtensions,
-                        extensions: cachedExtensions.data.fetchExtensions.extensions.map((extension) => {
-                            const isUpdatedExtension =
-                                extension.apkName === response.data?.updateExtension.extension?.apkName;
-                            if (!isUpdatedExtension) {
-                                return extension;
-                            }
+                        extensions: cachedExtensions.data.fetchExtensions.extensions
+                            .filter((extension) => {
+                                const isUpdatedExtension = id === extension.pkgName;
+                                return isUpdatedExtension && !isObsolete;
+                            })
+                            .map((extension) => {
+                                const isUpdatedExtension = id === extension.pkgName;
+                                if (!isUpdatedExtension) {
+                                    return extension;
+                                }
 
-                            return {
-                                ...extension,
-                                ...response.data?.updateExtension.extension,
-                            };
-                        }),
+                                return {
+                                    ...extension,
+                                    ...response.data?.updateExtension.extension,
+                                };
+                            }),
                     },
                 },
             };

src/components/ExtensionCard.tsx
@@ -110,13 +110,13 @@ export function ExtensionCard(props: IProps) {
             setInstalledState(state);
             switch (action) {
                 case ExtensionAction.INSTALL:
-                    await requestManager.updateExtension(pkgName, { install: true }).response;
+                    await requestManager.updateExtension(pkgName, { install: true, isObsolete }).response;
                     break;
                 case ExtensionAction.UNINSTALL:
-                    await requestManager.updateExtension(pkgName, { uninstall: true }).response;
+                    await requestManager.updateExtension(pkgName, { uninstall: true, isObsolete }).response;
                     break;
                 case ExtensionAction.UPDATE:
-                    await requestManager.updateExtension(pkgName, { update: true }).response;
+                    await requestManager.updateExtension(pkgName, { update: true, isObsolete }).response;
                     break;
                 default:
                     throw new Error(`Unexpected ExtensionAction "${action}"`);

commit a9b418f06a8d27bfd8d429fdc3fe17386d453675

Correctly update cache after updating an extension
After installing/uninstalling/updating an extension all other extension were removed from the list besides the updated extension

Regression introduced with 506e0aa0e38c0bcb26931b85ef1d6d688b2d95ca

src/lib/requests/RequestManager.ts
@@ -1100,8 +1100,12 @@ export class RequestManager {
                         ...cachedExtensions.data.fetchExtensions,
                         extensions: cachedExtensions.data.fetchExtensions.extensions
                             .filter((extension) => {
+                                if (!isObsolete) {
+                                    return true;
+                                }
+
                                 const isUpdatedExtension = id === extension.pkgName;
-                                return isUpdatedExtension && !isObsolete;
+                                return !isUpdatedExtension;
                             })
                             .map((extension) => {
                                 const isUpdatedExtension = id === extension.pkgName;