Suwayomi/Suwayomi-WebUI

commit db5d3ff7c0062257f7096df906bdcee84a98c17e

Remove "directLink" prop (#488)
Is not required anymore since "Link" of react-router-dom is a "<a>" element now anyway.
It also caused the following warning

"react-dom.development.js:86  Warning: React does not recognize the `directLink` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `directlink` instead. If you accidentally passed it from a parent component, remove it from the DOM element"

src/screens/settings/Backup.tsx
@@ -161,7 +161,7 @@ export function Backup() {
     return (
         <>
             <List sx={{ padding: 0 }}>
-                <ListItemLink to={requestManager.getExportBackupUrl()} directLink>
+                <ListItemLink to={requestManager.getExportBackupUrl()}>
                     <ListItemText
                         primary={t('settings.backup.label.create_backup')}
                         secondary={t('settings.backup.label.create_backup_info')}

src/components/util/ListItemLink.tsx
@@ -9,15 +9,6 @@
 import { Link } from 'react-router-dom';
 import { ListItemButton, ListItemButtonProps } from '@mui/material';
 
-export function ListItemLink(props: ListItemButtonProps<typeof Link, { directLink?: boolean }>) {
-    const { directLink, to } = props;
-    if (directLink) {
-        if (typeof to !== 'string') {
-            throw new Error('ListItemLink: "to" has to be a string in case it is a directLink');
-        }
-
-        return <ListItemButton component="a" href={to} {...props} />;
-    }
-
+export function ListItemLink(props: ListItemButtonProps<typeof Link>) {
     return <ListItemButton component={Link} {...props} />;
 }

src/screens/settings/About.tsx
@@ -93,16 +93,16 @@ export function About() {
                     </ListSubheader>
                 }
             >
-                <ListItemLink directLink to={aboutServer.github}>
+                <ListItemLink to={aboutServer.github}>
                     <ListItemText primary={t('settings.about.server.label.github')} secondary={aboutServer.github} />
                 </ListItemLink>
-                <ListItemLink directLink to="https://github.com/Suwayomi/Tachidesk-WebUI">
+                <ListItemLink to="https://github.com/Suwayomi/Tachidesk-WebUI">
                     <ListItemText
                         primary={t('settings.about.webui.label.github')}
                         secondary="https://github.com/Suwayomi/Tachidesk-WebUI"
                     />
                 </ListItemLink>
-                <ListItemLink directLink to={aboutServer.discord}>
+                <ListItemLink to={aboutServer.discord}>
                     <ListItemText primary={t('global.label.discord')} secondary={aboutServer.discord} />
                 </ListItemLink>
             </List>

commit 0d15dc06b6c1b7880f64a2dc1d56a0bc5a23d7fa

Handle backup creation on same domain as server
In case the webUI is hosted by the server the backup creation caused the app to navigate to the backup url which resulted in an empty page

Regression introduced with db5d3ff7c0062257f7096df906bdcee84a98c17e

src/screens/settings/Backup.tsx
@@ -227,7 +227,7 @@ export function Backup() {
     return (
         <>
             <List sx={{ padding: 0 }}>
-                <ListItemLink to={requestManager.getExportBackupUrl()}>
+                <ListItemLink to={requestManager.getExportBackupUrl()} download target="_self">
                     <ListItemText
                         primary={t('settings.backup.action.create.label.title')}
                         secondary={t('settings.backup.action.create.label.description')}