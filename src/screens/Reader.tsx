Suwayomi/Suwayomi-WebUI

commit 7caea4e5262ecbd42a06a37de57623a2cafa9b61

Download ahead only in case current and next chapter are downloaded

src/screens/Reader.tsx
@@ -188,6 +188,16 @@ export function Reader() {
     const { settings: metadataSettings } = useMetadataServerSettings();
 
     const updateChapter = (patch: UpdateChapterPatchInput) => {
+        const getChapterFromCache = (id: number) =>
+            requestManager.graphQLClient.client.cache.readFragment<TChapter>({
+                id: requestManager.graphQLClient.client.cache.identify({
+                    __typename: 'ChapterType',
+                    id,
+                }),
+                fragment: FULL_CHAPTER_FIELDS,
+                fragmentName: 'FULL_CHAPTER_FIELDS',
+            });
+
         const isAutoDeletionEnabled = !!patch.isRead && !!metadataSettings.deleteChaptersWhileReading;
 
         const getChapterIdToDelete = () => {
@@ -204,14 +214,7 @@ export function Reader() {
                 return -1;
             }
 
-            const chapterToDeleteUpToDateData = requestManager.graphQLClient.client.cache.readFragment<TChapter>({
-                id: requestManager.graphQLClient.client.cache.identify({
-                    __typename: 'ChapterType',
-                    id: chapterToDelete.id,
-                }),
-                fragment: FULL_CHAPTER_FIELDS,
-                fragmentName: 'FULL_CHAPTER_FIELDS',
-            });
+            const chapterToDeleteUpToDateData = getChapterFromCache(chapterToDelete.id);
 
             const shouldDeleteChapter =
                 chapterToDeleteUpToDateData?.isDownloaded &&
@@ -223,8 +226,17 @@ export function Reader() {
             return chapterToDelete.id;
         };
 
+        const currentChapter = getChapterFromCache(chapter.id);
+        const nextChapterId = mangaChapters?.[Number(chapterIndex) + 1]?.id;
+        const nextChapter = nextChapterId ? getChapterFromCache(nextChapterId) : null;
+
         const shouldDownloadAhead =
-            chapter.manga.inLibrary && !chapter.isRead && !!patch.isRead && isDownloadAheadEnabled;
+            isDownloadAheadEnabled &&
+            chapter.manga.inLibrary &&
+            !chapter.isRead &&
+            !!patch.isRead &&
+            !!currentChapter?.isDownloaded &&
+            !!nextChapter?.isDownloaded;
 
         requestManager
             .updateChapter(chapter.id, {

commit 0edec685f9d96eb0cdf94d6e176b730fc743fb65

Correctly select next chapter id for download ahead (#596)
Chapters are being fetched sorted by descending source order, thus, the next chapters index is less than the current one

Regression introduced with 7caea4e5262ecbd42a06a37de57623a2cafa9b61

src/screens/Reader.tsx
@@ -224,7 +224,7 @@ export function Reader() {
         };
 
         const currentChapter = getChapterFromCache(chapter.id);
-        const nextChapterId = mangaChapters?.[Number(chapterIndex) + 1]?.id;
+        const nextChapterId = mangaChapters?.[(mangaChapters?.length ?? 0) - Number(chapterIndex) - 1]?.id;
         const nextChapter = nextChapterId ? getChapterFromCache(nextChapterId) : null;
 
         const shouldDownloadAhead =