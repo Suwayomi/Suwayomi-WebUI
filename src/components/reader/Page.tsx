Suwayomi/Suwayomi-WebUI

commit 15825fbe530bf1de58565db63545341b84225da9

Prevent pages from being bigger than the 100% in width (#573)

src/components/reader/Page.tsx
@@ -41,7 +41,7 @@ export function imageStyle(settings: IReaderSettings): any {
             marginLeft: isHorizontal ? '7px' : 0,
             marginRight: isHorizontal ? '7px' : 0,
             width: 'auto',
-            minHeight: '100vh',
+            maxWidth: settings.fitPageToWindow ? 'calc(100vw - (100vw - 100%))' : undefined,
             height: 'auto',
             maxHeight: '100vh',
             objectFit: 'contain',

commit 5287d3f7b5da59c2c2181ac6c5ec925ddf2c372c

Fix size of pages in continues reader mode
In case "fit to page" was enabled, the styling got incorrectly applied to horizontal pages. Causing the pages to be small

Regression introduced with 15825fbe530bf1de58565db63545341b84225da9

src/components/reader/Page.tsx
@@ -24,8 +24,9 @@ export function imageStyle(settings: IReaderSettings): any {
             marginRight: isHorizontal ? '7px' : 0,
             marginBottom: settings.readerType === 'ContinuesVertical' ? '15px' : 0,
             width: 'auto',
-            maxWidth: settings.fitPageToWindow ? 'calc(100vw - (100vw - 100%))' : undefined,
+            maxWidth: settings.fitPageToWindow && !isHorizontal ? 'calc(100vw - (100vw - 100%))' : undefined,
             height: 'auto',
+            minHeight: isHorizontal ? '100vh' : undefined,
             maxHeight: '100vh',
             objectFit: 'contain',
         };