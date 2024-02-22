Suwayomi/Suwayomi-WebUI

commit ab0ecf4da0a2511fd0a8d8885bce07e53dfd5840

Center page number correctly (#616)

src/screens/Reader.tsx
@@ -413,6 +413,7 @@ export function Reader() {
                 display: 'flex',
                 flexDirection: 'column',
                 alignContent: 'center',
+                alignItems: 'center',
                 justifyContent: 'center',
                 minWidth: settings.staticNav
                     ? 'calc((100vw - (100vw - 100%)) - 300px)'

src/components/reader/PageNumber.tsx
@@ -24,7 +24,6 @@ export function PageNumber(props: IProps) {
                 display: settings.showPageNumber ? 'block' : 'none',
                 position: 'fixed',
                 bottom: '50px',
-                right: settings.staticNav ? 'calc((100vw - 325px)/2)' : 'calc((100vw - 25px)/2)',
                 padding: '2px',
                 paddingLeft: '4px',
                 paddingRight: '4px',

commit 1c58329fc53ab84b13d58528cbfa1293f20d70eb

Use full available width for reader component
Due to horizontally centering the flex-box items via "alignItems", the items did not stretch anymore, which is the default value for "alignItems" and thus, did not take up the available width.

Regression introduced with ab0ecf4da0a2511fd0a8d8885bce07e53dfd5840

src/screens/Reader.tsx
@@ -423,19 +423,21 @@ export function Reader() {
             }}
         >
             <PageNumber settings={settings} curPage={curPage} pageCount={chapter.pageCount} />
-            <ReaderComponent
-                key={chapter.id}
-                pages={pages}
-                pageCount={chapter.pageCount}
-                setCurPage={setCurPage}
-                initialPage={initialPage}
-                curPage={curPage}
-                settings={settings}
-                manga={manga}
-                chapter={chapter}
-                nextChapter={loadNextChapter}
-                prevChapter={loadPrevChapter}
-            />
+            <Box sx={{ alignSelf: 'stretch' }}>
+                <ReaderComponent
+                    key={chapter.id}
+                    pages={pages}
+                    pageCount={chapter.pageCount}
+                    setCurPage={setCurPage}
+                    initialPage={initialPage}
+                    curPage={curPage}
+                    settings={settings}
+                    manga={manga}
+                    chapter={chapter}
+                    nextChapter={loadNextChapter}
+                    prevChapter={loadPrevChapter}
+                />
+            </Box>
         </Box>
     );
 }