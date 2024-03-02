88ab6f36e6702940911c480f5c88cdfd768d5cbf
schroda <50052685+schroda@users.noreply.github.com>
2024-03-02 15:07:25 +0100

    Use "Mangas::removeFromLibrary"

--- src/components/manga/MangaDetails.tsx
+++ src/components/manga/MangaDetails.tsx
@@ -175,7 +175,7 @@ export const MangaDetails: React.FC<IProps> = ({ manga }) => {
     const { t } = useTranslation();
 
     const {
-        settings: { showAddToLibraryCategorySelectDialog, removeMangaFromCategories },
+        settings: { showAddToLibraryCategorySelectDialog },
         loading: areSettingsLoading,
     } = useMetadataServerSettings();
 
@@ -229,16 +229,7 @@ export const MangaDetails: React.FC<IProps> = ({ manga }) => {
     };
 
     const removeFromLibrary = () => {
-        Promise.all([
-            requestManager.updateManga(manga.id, {
-                updateManga: { inLibrary: false },
-                updateMangaCategories: removeMangaFromCategories ? { clearCategories: true } : undefined,
-            }).response,
-        ])
-            .then(() => makeToast(t('library.info.label.removed_from_library'), 'success'))
-            .catch(() => {
-                makeToast(t('library.error.label.remove_from_library'), 'error');
-            });
+        Mangas.removeFromLibrary([manga.id]).catch(defaultPromiseErrorHandler('MangaDetails::removeFromLibrary'));
     };
 
     return (
