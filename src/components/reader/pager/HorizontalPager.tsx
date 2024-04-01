Suwayomi/Suwayomi-WebUI

commit 0c1abebaf47b0cd92c0f32cad316e867e96e322c

Disconnect resize observer after restoring initial page (#660)
Everytime the reader width got changed, the resize observer triggered the initial page to get scrolled into view again

src/components/reader/pager/HorizontalPager.tsx
@@ -115,6 +115,7 @@ export function HorizontalPager(props: IReaderProps) {
 
         const resizeObserver = new ResizeObserver(() => {
             initialPageElement.scrollIntoView({ inline: 'center' });
+            resizeObserver.disconnect();
         });
         resizeObserver.observe(initialPageElement);
 

src/components/reader/pager/VerticalPager.tsx
@@ -173,6 +173,7 @@ export function VerticalPager(props: IReaderProps) {
 
         const resizeObserver = new ResizeObserver(() => {
             initialPageElement.scrollIntoView();
+            resizeObserver.disconnect();
         });
         resizeObserver.observe(initialPageElement);
 

commit 286401a254ccbe217a0d5edbe1d047b171a22389

Wait for initial page to be rendered before scrolling it into view
The ResizeObserver triggers the first time when the element has a height of 0, which apparently did not work with scrolling it into view.

Due to immediately disconnecting the observer, the next resize event, which would successfully scroll the page into view, was never handled

Regression introduced with 0c1abebaf47b0cd92c0f32cad316e867e96e322c

src/components/reader/pager/HorizontalPager.tsx
@@ -114,6 +114,10 @@ export function HorizontalPager(props: IReaderProps) {
         }
 
         const resizeObserver = new ResizeObserver(() => {
+            if (!initialPageElement.offsetHeight) {
+                return;
+            }
+
             initialPageElement.scrollIntoView({ inline: 'center' });
             resizeObserver.disconnect();
         });

src/components/reader/pager/VerticalPager.tsx
@@ -172,6 +172,10 @@ export function VerticalPager(props: IReaderProps) {
         }
 
         const resizeObserver = new ResizeObserver(() => {
+            if (!initialPageElement.offsetHeight) {
+                return;
+            }
+
             initialPageElement.scrollIntoView();
             resizeObserver.disconnect();
         });