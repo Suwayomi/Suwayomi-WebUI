Suwayomi/Suwayomi-WebUI

commit aab3e25031babe13c5df4e64aa57e61a8d8db80c

Feature/improve restoring grid snapshots (#713)
* Update local storage functions

* Update local storage hooks

* Add session storage

* Improve storing virtuoso grid snapshot

* Use "useSyncExternalStore" for browser storage access

* Get rid of location replacements in "AppbarSearch"

Use session storage instead to prevent reloading the page all the time due to changing the location

* Get rid of location replacements in "SourceMangas"

Use session storage instead to prevent reloading the page all the time due to changing the location

src/components/MangaGrid.tsx
@@ -10,15 +10,16 @@ import React, { ForwardedRef, forwardRef, useEffect, useLayoutEffect, useMemo, u
 import Grid, { GridTypeMap } from '@mui/material/Grid';
 import { Box, Typography } from '@mui/material';
 import { GridItemProps, GridStateSnapshot, VirtuosoGrid } from 'react-virtuoso';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { EmptyView } from '@/components/util/EmptyView';
 import { LoadingPlaceholder } from '@/components/util/LoadingPlaceholder';
 import { MangaCard, MangaCardProps } from '@/components/MangaCard';
 import { GridLayout } from '@/components/context/LibraryOptionsContext';
-import { useLocalStorage } from '@/util/useLocalStorage';
+import { useLocalStorage, useSessionStorage } from '@/util/useStorage.tsx';
 import { TManga, TPartialManga } from '@/typings.ts';
 import { SelectableCollectionReturnType } from '@/components/collection/useSelectableCollection.ts';
 import { DEFAULT_FULL_FAB_HEIGHT } from '@/components/util/StyledFab.tsx';
+import { AppStorage } from '@/util/AppStorage.ts';
 
 const GridContainer = React.forwardRef<HTMLDivElement, GridTypeMap['props']>(({ children, ...props }, ref) => (
     <Grid {...props} ref={ref} container sx={{ paddingLeft: '5px', paddingRight: '13px' }}>
@@ -144,8 +145,9 @@ const VerticalGrid = forwardRef(
         ref: ForwardedRef<HTMLDivElement | null>,
     ) => {
         const location = useLocation<{ snapshot?: GridStateSnapshot }>();
-        const navigate = useNavigate();
-        const { snapshot } = location.state ?? {};
+
+        const snapshotSessionKey = `MangaGrid-snapshot-location-${location.key}`;
+        const [snapshot] = useSessionStorage<GridStateSnapshot | undefined>(snapshotSessionKey, undefined);
 
         const persistGridStateTimeout = useRef<NodeJS.Timeout | undefined>();
         const persistGridState = (gridState: GridStateSnapshot) => {
@@ -158,10 +160,7 @@ const VerticalGrid = forwardRef(
                     return;
                 }
 
-                navigate(
-                    { pathname: '', search: location.search },
-                    { replace: true, state: { ...location.state, snapshot: gridState } },
-                );
+                AppStorage.session.setItem(snapshotSessionKey, gridState);
             }, 250);
         };
         useEffect(() => clearTimeout(persistGridStateTimeout.current), [location.key, persistGridStateTimeout.current]);

src/screens/SourceMangas.tsx
@@ -23,7 +23,6 @@ import {
     AbortableApolloUseMutationPaginatedResponse,
     SPECIAL_ED_SOURCES,
 } from '@/lib/requests/RequestManager.ts';
-import { useDebounce } from '@/util/useDebounce.ts';
 import { useLibraryOptionsContext } from '@/components/context/LibraryOptionsContext';
 import { SourceGridLayout } from '@/components/source/SourceGridLayout';
 import { AppbarSearch } from '@/components/util/AppbarSearch';
@@ -35,6 +34,7 @@ import {
 } from '@/lib/graphql/generated/graphql.ts';
 import { NavBarContext, useSetDefaultBackTo } from '@/components/context/NavbarContext.tsx';
 import { useMetadataServerSettings } from '@/lib/metadata/metadataServerSettings.ts';
+import { useSessionStorage } from '@/util/useStorage.tsx';
 
 const ContentTypeMenu = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -215,17 +215,12 @@ export function SourceMangas() {
     const { sourceId } = useParams<{ sourceId: string }>();
 
     const navigate = useNavigate();
-    const { search } = useLocation();
-    const {
-        contentType: currentLocationContentType = SourceContentType.POPULAR,
-        filtersToApply: currentLocationFiltersToApply = [],
-        clearCache = false,
-    } = useLocation<{
-        contentType: SourceContentType;
-        filtersToApply: IPos[];
-        clearCache: boolean;
-        search: string;
-    }>().state ?? {};
+    const { key: locationKey, state: locationState } = useLocation();
+    const { contentType: initialContentType = SourceContentType.POPULAR, clearCache = false } =
+        useLocation<{
+            contentType: SourceContentType;
+            clearCache: boolean;
+        }>().state ?? {};
 
     useSetDefaultBackTo('sources');
 
@@ -241,13 +236,19 @@ export function SourceMangas() {
 
     const { options } = useLibraryOptionsContext();
     const [query] = useQueryParam('query', StringParam);
-    const [dialogFiltersToApply, setDialogFiltersToApply] = useState<IPos[]>(currentLocationFiltersToApply);
-    const [filtersToApply, setFiltersToApply] = useState<IPos[]>(currentLocationFiltersToApply);
-    const searchTerm = useDebounce(query, 1000);
+    const [filtersToApply, setFiltersToApply] = useSessionStorage<IPos[]>(
+        `source-mangas-location-${locationKey}-${sourceId}-filters`,
+        [],
+    );
+    const [dialogFiltersToApply, setDialogFiltersToApply] = useState<IPos[]>(filtersToApply);
     const [resetScrollPosition, setResetScrollPosition] = useState(false);
-    const [contentType, setContentType] = useState(currentLocationContentType);
+    const [contentType, setContentType] = useSessionStorage(
+        `source-mangas-location-${locationKey}-${sourceId}-content-type`,
+        query ? SourceContentType.SEARCH : initialContentType,
+    );
+
     const [loadPage, { data, isLoading: loading, size: lastPageNum, abortRequest, filteredOutAllItemsOfFetchedPage }] =
-        useSourceManga(sourceId, contentType, searchTerm, filtersToApply, 1, hideLibraryEntries);
+        useSourceManga(sourceId, contentType, query, filtersToApply, 1, hideLibraryEntries);
     const isLoading = loading || filteredOutAllItemsOfFetchedPage;
     const mangas = data?.fetchSourceManga.mangas ?? [];
     const hasNextPage = data?.fetchSourceManga.hasNextPage ?? false;
@@ -268,50 +269,36 @@ export function SourceMangas() {
     ) : undefined;
 
     const updateContentType = useCallback(
-        (
-            newContentType: SourceContentType,
-            { updateLocationState = true, search: newSearch }: { updateLocationState?: boolean; search?: string } = {},
-        ) => {
-            if (updateLocationState) {
+        (newContentType: SourceContentType, newSearch?: string | null) => {
+            setContentType(newContentType);
+            setResetScrollPosition(true);
+
+            if (query && !newSearch) {
                 navigate(
-                    { pathname: '', search: newSearch },
                     {
-                        replace: true,
-                        state: {
-                            contentType: newContentType,
-                        },
+                        pathname: '',
+                    },
+                    {
+                        state: { ...locationState, contentType: newContentType },
                     },
                 );
             }
-
-            setContentType(newContentType);
-            setResetScrollPosition(true);
         },
-        [setContentType],
+        [setContentType, query],
     );
 
     const updateLocationFilters = useCallback(
         (updatedFilters: IPos[]) => {
             if (contentType === SourceContentType.SEARCH) {
-                navigate(
-                    { pathname: '', search },
-                    {
-                        replace: true,
-                        state: {
-                            contentType,
-                            filtersToApply: updatedFilters,
-                        },
-                    },
-                );
+                setFiltersToApply(updatedFilters);
             }
         },
-        [contentType, search],
+        [contentType, query],
     );
 
-    const isSearchTermAvailable = searchTerm && query?.length;
-    const setSearchContentType = isSearchTermAvailable && contentType !== SourceContentType.SEARCH;
+    const setSearchContentType = !!query && contentType !== SourceContentType.SEARCH;
     if (setSearchContentType) {
-        updateContentType(SourceContentType.SEARCH, { search });
+        updateContentType(SourceContentType.SEARCH, query);
     }
 
     const loadMore = useCallback(() => {
@@ -346,10 +333,6 @@ export function SourceMangas() {
         }
 
         requestManager.clearBrowseCacheFor(sourceId);
-        navigate('', {
-            replace: true,
-            state: { contentType: currentLocationContentType, filters: currentLocationFiltersToApply },
-        });
     }, [clearCache]);
 
     useEffect(
@@ -363,7 +346,7 @@ export function SourceMangas() {
             abortRequest(new Error(`SourceMangas(${sourceId}): search string changed`));
             setResetScrollPosition(true);
         },
-        [searchTerm],
+        [query],
     );
 
     useEffect(() => {
@@ -426,7 +409,7 @@ export function SourceMangas() {
                 <ContentTypeButton
                     variant={contentType === SourceContentType.SEARCH ? 'contained' : 'outlined'}
                     startIcon={<FilterListIcon />}
-                    onClick={() => updateContentType(SourceContentType.SEARCH)}
+                    onClick={() => updateContentType(SourceContentType.SEARCH, query)}
                 >
                     {t('global.button.filter')}
                 </ContentTypeButton>

src/components/chapter/util.tsx
@@ -15,7 +15,7 @@ import {
     TChapter,
     TranslationKey,
 } from '@/typings.ts';
-import { useReducerLocalStorage } from '@/util/useLocalStorage.tsx';
+import { useReducerLocalStorage } from '@/util/useStorage.tsx';
 
 const defaultChapterOptions: ChapterListOptions = {
     active: false,

src/components/context/AppContext.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { createTheme } from '@/theme';
-import { useLocalStorage } from '@/util/useLocalStorage';
+import { useLocalStorage } from '@/util/useStorage.tsx';
 import { DarkTheme } from '@/components/context/DarkTheme';
 import { NavBarContextProvider } from '@/components/navbar/NavBarContextProvider';
 import { LibraryOptionsContextProvider } from '@/components/library/LibraryOptionsProvider';

src/components/library/LibraryOptionsProvider.tsx
@@ -8,7 +8,7 @@
 
 import React, { useMemo } from 'react';
 import { LibraryOptions } from '@/typings';
-import { useLocalStorage } from '@/util/useLocalStorage';
+import { useLocalStorage } from '@/util/useStorage.tsx';
 import { LibraryOptionsContext, DefaultLibraryOptions } from '@/components/context/LibraryOptionsContext';
 
 interface IProps {

src/components/settings/ServerUpdateChecker.tsx
@@ -16,7 +16,7 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { requestManager } from '@/lib/requests/RequestManager.ts';
 import { getVersion } from '@/screens/settings/About.tsx';
-import { useLocalStorage } from '@/util/useLocalStorage.tsx';
+import { useLocalStorage } from '@/util/useStorage.tsx';
 import { defaultPromiseErrorHandler } from '@/util/defaultPromiseErrorHandler.ts';
 
 const UPDATE_CHECK_INTERVAL = 1000 * 60 * 60 * 24; // 1 day

src/components/util/AppbarSearch.tsx
@@ -11,35 +11,44 @@ import SearchIcon from '@mui/icons-material/Search';
 import { IconButton, Tooltip } from '@mui/material';
 import { useQueryParam, StringParam } from 'use-query-params';
 import { useTranslation } from 'react-i18next';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { SearchTextField } from '@/components/atoms/SearchTextField.tsx';
+import { useSessionStorage } from '@/util/useStorage.tsx';
 
 interface IProps {
-    autoOpen?: boolean;
+    isClosable?: boolean;
 }
 
-const defaultProps = {
-    autoOpen: false,
-};
-
 export const AppbarSearch: React.FunctionComponent<IProps> = (props) => {
-    const { autoOpen } = props;
+    const { isClosable = true } = props;
 
     const { t } = useTranslation();
 
-    const { pathname, search: locationSearch, state: fullLocationState } = useLocation<{ wasSearchOpen?: boolean }>();
-    const { wasSearchOpen, ...locationState } = fullLocationState ?? {};
-
-    const navigate = useNavigate();
+    const [prevLocationKey, setPrevLocationKey] = useState<string>();
+    const location = useLocation();
 
     const [query, setQuery] = useQueryParam('query', StringParam);
-    const [searchOpen, setSearchOpen] = useState(!!query);
+    const [isSearchOpen, setIsSearchOpen] = useState(!isClosable || !!query);
     const inputRef = React.useRef<HTMLInputElement>();
 
     const [searchString, setSearchString] = useState(query ?? '');
 
+    const [locationQuery, setLocationQuery] = useSessionStorage<string | null>(`appbarsearch-location-${location.key}`);
+    if (prevLocationKey !== location.key) {
+        setPrevLocationKey(location.key);
+        setLocationQuery(query);
+        setSearchString(locationQuery ?? '');
+        setIsSearchOpen(!isClosable || !!locationQuery);
+    }
+
+    const isOpen = isSearchOpen || !!locationQuery;
+
     const updateSearchOpenState = (open: boolean) => {
-        setSearchOpen(open);
+        if (!isClosable) {
+            return;
+        }
+
+        setIsSearchOpen(open);
 
         // try to focus input component since in case of navigating to the previous/next page in the browser history
         // the "openSearch" state might not change and thus, won't trigger a focus
@@ -54,6 +63,7 @@ export const AppbarSearch: React.FunctionComponent<IProps> = (props) => {
         }
 
         setQuery(newQuery);
+        updateSearchOpenState(false);
     }
 
     const cancelSearch = () => {
@@ -73,46 +83,6 @@ export const AppbarSearch: React.FunctionComponent<IProps> = (props) => {
     };
 
     useEffect(() => {
-        if ((autoOpen && wasSearchOpen === undefined) || (wasSearchOpen && query)) {
-            updateSearchOpenState(true);
-            return;
-        }
-
-        updateSearchOpenState(false);
-    }, [autoOpen, pathname]);
-
-    useEffect(() => {
-        if (!searchOpen || !inputRef.current) {
-            return;
-        }
-
-        inputRef.current.focus();
-    }, [searchOpen, inputRef.current]);
-
-    useEffect(() => {
-        if (wasSearchOpen === searchOpen) {
-            return;
-        }
-
-        navigate(
-            { pathname, search: locationSearch },
-            { replace: true, state: { ...locationState, wasSearchOpen: searchOpen } },
-        );
-    }, [searchOpen]);
-
-    useEffect(() => {
-        if (query === undefined && searchString !== undefined) {
-            setSearchString('');
-            return;
-        }
-
-        if (query && searchString !== query) {
-            setSearchString(query);
-            updateSearchOpenState(true);
-        }
-    }, [query]);
-
-    useEffect(() => {
         window.addEventListener('keydown', handleKeyboardEvent);
 
         return () => {
@@ -120,9 +90,10 @@ export const AppbarSearch: React.FunctionComponent<IProps> = (props) => {
         };
     }, [handleKeyboardEvent]);
 
-    if (searchOpen) {
+    if (isOpen) {
         return (
             <SearchTextField
+                autoFocus
                 variant="standard"
                 value={searchString}
                 onCancel={cancelSearch}
@@ -146,5 +117,3 @@ export const AppbarSearch: React.FunctionComponent<IProps> = (props) => {
         </Tooltip>
     );
 };
-
-AppbarSearch.defaultProps = defaultProps;

src/lib/requests/client/BaseClient.ts
@@ -6,7 +6,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import * as storage from '@/util/localStorage.tsx';
+import { AppStorage } from '@/util/AppStorage.ts';
 
 export abstract class BaseClient<Client, ClientConfig, Fetcher> {
     protected client!: Client;
@@ -22,7 +22,7 @@ export abstract class BaseClient<Client, ClientConfig, Fetcher> {
 
         // if port is 3000 it's probably running from webpack development server
         const inferredPort = port === '3000' ? '4567' : port;
-        return storage.getItem('serverBaseURL', `${protocol}//${hostname}:${inferredPort}`);
+        return AppStorage.local.getItemParsed('serverBaseURL', `${protocol}//${hostname}:${inferredPort}`);
     }
 
     protected abstract createClient(): void;

commit 1f227ce88620bca6f1263544be980e4e6a4b06d9

Scroll to top when changing shown data in source browse
The current scroll position was preserved when changing the shown content type (browse, latest, filters) or when changing the filters.

Regression introduced with aab3e25031babe13c5df4e64aa57e61a8d8db80c

src/components/MangaGrid.tsx
@@ -126,6 +126,9 @@ const HorizontalGrid = forwardRef(
     ),
 );
 
+export const getGridSnapshotKey = (location: ReturnType<typeof useLocation>) =>
+    `MangaGrid-snapshot-location-${location.key}`;
+
 const VerticalGrid = forwardRef(
     (
         {
@@ -148,7 +151,7 @@ const VerticalGrid = forwardRef(
     ) => {
         const location = useLocation<{ snapshot?: GridStateSnapshot }>();
 
-        const snapshotSessionKey = `MangaGrid-snapshot-location-${location.key}`;
+        const snapshotSessionKey = getGridSnapshotKey(location);
         const [snapshot] = useSessionStorage<GridStateSnapshot | undefined>(snapshotSessionKey, undefined);
 
         const persistGridStateTimeout = useRef<NodeJS.Timeout | undefined>();

src/screens/SourceMangas.tsx
@@ -38,6 +38,8 @@ import {
 import { NavBarContext, useSetDefaultBackTo } from '@/components/context/NavbarContext.tsx';
 import { useMetadataServerSettings } from '@/lib/metadata/metadataServerSettings.ts';
 import { useSessionStorage } from '@/util/useStorage.tsx';
+import { AppStorage } from '@/util/AppStorage.ts';
+import { getGridSnapshotKey } from '@/components/MangaGrid.tsx';
 
 const ContentTypeMenu = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -218,7 +220,8 @@ export function SourceMangas() {
     const { sourceId } = useParams<{ sourceId: string }>();
 
     const navigate = useNavigate();
-    const { key: locationKey, state: locationState } = useLocation();
+    const location = useLocation();
+    const { key: locationKey, state: locationState } = location;
     const { contentType: initialContentType = SourceContentType.POPULAR, clearCache = false } =
         useLocation<{
             contentType: SourceContentType;
@@ -248,7 +251,6 @@ export function SourceMangas() {
         currentFiltersToApply ?? [],
     );
     const [dialogFiltersToApply, setDialogFiltersToApply] = useState<IPos[]>(filtersToApply);
-    const [resetScrollPosition, setResetScrollPosition] = useState(false);
     const [currentContentType, setCurrentContentType] = useSessionStorage<SourceContentType | undefined>(
         `source-mangas-${sourceId}-content-type`,
         initialContentType,
@@ -266,9 +268,15 @@ export function SourceMangas() {
         [sourceId],
     );
 
+    const scrollToTop = useCallback(() => {
+        AppStorage.session.setItem(getGridSnapshotKey(location), undefined);
+        window.scrollTo(0, 0);
+    }, [locationKey]);
+
     const setFiltersToApply = (filters: IPos[]) => {
         setCurrentFiltersToApply(filters);
         setLocationFiltersToApply(filters);
+        scrollToTop();
     };
 
     const setContentType = (newContentType: SourceContentType) => {
@@ -300,7 +308,7 @@ export function SourceMangas() {
     const updateContentType = useCallback(
         (newContentType: SourceContentType, newSearch?: string | null) => {
             setContentType(newContentType);
-            setResetScrollPosition(true);
+            scrollToTop();
 
             if (query && !newSearch) {
                 navigate(
@@ -313,16 +321,7 @@ export function SourceMangas() {
                 );
             }
         },
-        [setContentType, query],
-    );
-
-    const updateLocationFilters = useCallback(
-        (updatedFilters: IPos[]) => {
-            if (contentType === SourceContentType.SEARCH) {
-                setFiltersToApply(updatedFilters);
-            }
-        },
-        [contentType, query],
+        [setContentType, query, scrollToTop],
     );
 
     const setSearchContentType = !!query && contentType !== SourceContentType.SEARCH;
@@ -338,12 +337,10 @@ export function SourceMangas() {
         loadPage(lastPageNum + 1);
     }, [lastPageNum, hasNextPage, contentType]);
 
-    const resetFilters = useCallback(async () => {
+    const resetFilters = useCallback(() => {
         setDialogFiltersToApply([]);
         setFiltersToApply([]);
-        updateLocationFilters([]);
-        setResetScrollPosition(true);
-    }, [sourceId, contentType, updateLocationFilters]);
+    }, [sourceId, contentType]);
 
     useEffect(() => {
         if (filteredOutAllItemsOfFetchedPage && hasNextPage && !loading) {
@@ -373,7 +370,7 @@ export function SourceMangas() {
             // with strict mode + dev mode the first request will be aborted. due to using SWR there won't be an
             // immediate second request since it's the same key. instead the "second" request will be the error handling of SWR
             abortRequest(new Error(`SourceMangas(${sourceId}): search string changed`));
-            setResetScrollPosition(true);
+            scrollToTop();
         },
         [query],
     );
@@ -405,15 +402,6 @@ export function SourceMangas() {
             setAction(null);
         };
     }, [t, source]);
-
-    useEffect(() => {
-        if (!resetScrollPosition) {
-            return;
-        }
-
-        window.scrollTo(0, 0);
-        setResetScrollPosition(false);
-    }, [resetScrollPosition]);
 
     return (
         <StyledGridWrapper hasContent={!!mangas.length}>
@@ -459,7 +447,6 @@ export function SourceMangas() {
                     updateFilterValue={setDialogFiltersToApply}
                     setTriggerUpdate={() => {
                         setFiltersToApply(dialogFiltersToApply);
-                        updateLocationFilters(dialogFiltersToApply);
                     }}
                     resetFilterValue={resetFilters}
                     update={dialogFiltersToApply}