Suwayomi/Suwayomi-WebUI

commit 3a7c93ea0666aa92f782b6d600a285e71781daf1

Remove referrers from links

src/screens/settings/ServerSettings.tsx
@@ -245,14 +245,14 @@ export const ServerSettings = () => {
                         primary={t('settings.server.cloudflare.flaresolverr.enabled.label.title')}
                         secondary={
                             <Trans i18nKey="settings.server.cloudflare.flaresolverr.enabled.label.description">
-                                See{' '}
+                                See
                                 <Link
                                     href="https://github.com/FlareSolverr/FlareSolverr?tab=readme-ov-file#installation"
                                     target="_blank"
                                     rel="noreferrer"
                                 >
                                     FlareSolverr
-                                </Link>{' '}
+                                </Link>
                                 for information on how to set it up
                             </Trans>
                         }

src/components/util/VersionUpdateInfoDialog.tsx
@@ -65,7 +65,7 @@ export const VersionUpdateInfoDialog = ({
             <DialogActions>
                 <Stack sx={{ width: '100%' }} direction="row" justifyContent={changelogUrl ? 'space-between' : 'end'}>
                     {changelogUrl && (
-                        <Button href={changelogUrl} target="_blank">
+                        <Button href={changelogUrl} target="_blank" rel="noreferrer">
                             {t('global.button.changelog')}
                         </Button>
                     )}
@@ -98,7 +98,13 @@ export const VersionUpdateInfoDialog = ({
                             )}
                         </PopupState>
                         {actionUrl ? (
-                            <Button disabled={disabled} variant="contained" href={actionUrl} target="_blank">
+                            <Button
+                                disabled={disabled}
+                                variant="contained"
+                                href={actionUrl}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
                                 {actionTitle}
                             </Button>
                         ) : (

src/components/util/WebUIUpdateChecker.tsx
@@ -156,7 +156,7 @@ export const WebUIUpdateChecker = () => {
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
-                <Button href={changelogUrl} target="_blank">
+                <Button href={changelogUrl} target="_blank" rel="noreferrer">
                     {t('global.button.changelog')}
                 </Button>
                 <Button

src/screens/Settings.tsx
@@ -140,7 +140,11 @@ export function Settings() {
                     secondary={
                         <>
                             <span>{t('settings.label.language_description')} </span>
-                            <Link href="https://hosted.weblate.org/projects/suwayomi/suwayomi-webui">
+                            <Link
+                                href="https://hosted.weblate.org/projects/suwayomi/suwayomi-webui"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
                                 {t('global.language.title.weblate')}
                             </Link>
                         </>

src/screens/SourceMangas.tsx
@@ -299,7 +299,7 @@ export function SourceMangas() {
     const messageExtra = isLocalSource ? (
         <>
             <span>{t('source.local_source.label.checkout')} </span>
-            <Link href="https://github.com/Suwayomi/Suwayomi-Server/wiki/Local-Source">
+            <Link href="https://github.com/Suwayomi/Suwayomi-Server/wiki/Local-Source" target="_blank" rel="noreferrer">
                 {t('source.local_source.label.guide')}
             </Link>
         </>

src/screens/settings/About.tsx
@@ -334,16 +334,16 @@ export function About() {
                     </ListSubheader>
                 }
             >
-                <ListItemLink to={aboutServer.github}>
+                <ListItemLink to={aboutServer.github} target="_blank" rel="noreferrer">
                     <ListItemText primary={t('settings.about.server.label.github')} secondary={aboutServer.github} />
                 </ListItemLink>
-                <ListItemLink to="https://github.com/Suwayomi/Suwayomi-WebUI">
+                <ListItemLink to="https://github.com/Suwayomi/Suwayomi-WebUI" target="_blank" rel="noreferrer">
                     <ListItemText
                         primary={t('settings.about.webui.label.github')}
                         secondary="https://github.com/Suwayomi/Suwayomi-WebUI"
                     />
                 </ListItemLink>
-                <ListItemLink to={aboutServer.discord}>
+                <ListItemLink to={aboutServer.discord} target="_blank" rel="noreferrer">
                     <ListItemText primary={t('global.label.discord')} secondary={aboutServer.discord} />
                 </ListItemLink>
             </List>

commit c6b99478f3b049054f214a72487012f51924bdc4

Fix missing flaresolverr link
The spaces are important so that i18n correctly converts the translation to the actual text.

Regression introduced with 3a7c93ea0666aa92f782b6d600a285e71781daf1

src/screens/settings/ServerSettings.tsx
@@ -304,14 +304,14 @@ export const ServerSettings = () => {
                         primary={t('settings.server.cloudflare.flaresolverr.enabled.label.title')}
                         secondary={
                             <Trans i18nKey="settings.server.cloudflare.flaresolverr.enabled.label.description">
-                                See
+                                See{' '}
                                 <Link
                                     href="https://github.com/FlareSolverr/FlareSolverr?tab=readme-ov-file#installation"
                                     target="_blank"
                                     rel="noreferrer"
                                 >
                                     FlareSolverr
-                                </Link>
+                                </Link>{' '}
                                 for information on how to set it up
                             </Trans>
                         }