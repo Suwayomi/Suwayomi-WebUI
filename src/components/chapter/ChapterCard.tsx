Suwayomi/Suwayomi-WebUI

commit 0ab6549a2092d1bcfe24b6604ab473e4a2b222d2

Limit lines of chapter names of ChapterCard

src/components/chapter/ChapterCard.tsx
@@ -28,6 +28,7 @@ import { DownloadType } from '@/lib/graphql/generated/graphql.ts';
 import { TChapter } from '@/typings.ts';
 import { ChapterActionMenuItems } from '@/components/chapter/ChapterActionMenuItems.tsx';
 import { Menu } from '@/components/menu/Menu.tsx';
+import { TypographyMaxLines } from '@/components/atoms/TypographyMaxLines.tsx';
 
 interface IProps {
     chapter: TChapter;
@@ -108,7 +109,7 @@ export const ChapterCard: React.FC<IProps> = (props: IProps) => {
                                     }}
                                 >
                                     <Stack direction="column" flex={1}>
-                                        <Typography variant="h5" component="h2">
+                                        <TypographyMaxLines variant="h5" component="h2">
                                             {chapter.isBookmarked && (
                                                 <BookmarkIcon
                                                     color="primary"
@@ -118,7 +119,7 @@ export const ChapterCard: React.FC<IProps> = (props: IProps) => {
                                             {showChapterNumber
                                                 ? `${t('chapter.title')} ${chapter.chapterNumber}`
                                                 : chapter.name}
-                                        </Typography>
+                                        </TypographyMaxLines>
                                         <Typography variant="caption">{chapter.scanlator}</Typography>
                                         <Typography variant="caption">
                                             {getDateString(Number(chapter.uploadDate ?? 0), true)}

commit af9e77524bf72a74e2da31a7c734d12f35c19db9

Prevent ChapterCard height change on selection
Due to the Checkbox being smaller than the vertical menu icon the ChapterCard was reduced in height, which caused the chapters in the list to jump up sightly.

This issue was introduced by 0ab6549a2092d1bcfe24b6604ab473e4a2b222d2 due to changing the line height of the chapter name

src/components/chapter/ChapterCard.tsx
@@ -128,26 +128,28 @@ export const ChapterCard: React.FC<IProps> = (props: IProps) => {
 
                                     {dc && <DownloadStateIndicator download={dc} />}
 
-                                    {selected === null ? (
-                                        <Tooltip title={t('global.button.options')}>
-                                            <IconButton
-                                                ref={menuButtonRef}
-                                                {...bindTrigger(popupState)}
-                                                onClick={(e) => handleClickOpenMenu(e, popupState.open)}
-                                                onTouchStart={(e) => handleClickOpenMenu(e, popupState.open)}
-                                                aria-label="more"
-                                                size="large"
+                                    <Stack sx={{ minHeight: '48px' }}>
+                                        {selected === null ? (
+                                            <Tooltip title={t('global.button.options')}>
+                                                <IconButton
+                                                    ref={menuButtonRef}
+                                                    {...bindTrigger(popupState)}
+                                                    onClick={(e) => handleClickOpenMenu(e, popupState.open)}
+                                                    onTouchStart={(e) => handleClickOpenMenu(e, popupState.open)}
+                                                    aria-label="more"
+                                                    size="large"
+                                                >
+                                                    <MoreVertIcon />
+                                                </IconButton>
+                                            </Tooltip>
+                                        ) : (
+                                            <Tooltip
+                                                title={t(selected ? 'global.button.deselect' : 'global.button.select')}
                                             >
-                                                <MoreVertIcon />
-                                            </IconButton>
-                                        </Tooltip>
-                                    ) : (
-                                        <Tooltip
-                                            title={t(selected ? 'global.button.deselect' : 'global.button.select')}
-                                        >
-                                            <Checkbox checked={selected} />
-                                        </Tooltip>
-                                    )}
+                                                <Checkbox checked={selected} />
+                                            </Tooltip>
+                                        )}
+                                    </Stack>
                                 </CardContent>
                             </CardActionArea>
                         </Card>