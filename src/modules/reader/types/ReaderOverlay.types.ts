a2e9036967c66148b5a48a17a687b3666c2c8965
schroda <50052685+schroda@users.noreply.github.com>
2024-09-28 03:24:59 +0200

    Reader overlay mobile header

---
+++ src/modules/reader/types/ReaderOverlay.types.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { ChapterType, MangaType } from '@/lib/graphql/generated/graphql.ts';
+import { ChapterBookmarkInfo, ChapterRealUrlInfo } from '@/modules/chapter/services/Chapters.ts';
+import { MangaIdInfo } from '@/modules/manga/Manga.types.ts';
+
+export interface BaseReaderOverlayProps {
+    isVisible: boolean;
+    setIsVisible: (visible: boolean) => void;
+}
+
+export interface MobileHeaderProps extends Pick<BaseReaderOverlayProps, 'isVisible'> {
+    manga: MangaIdInfo & Pick<MangaType, 'title'>;
+    chapter: Pick<ChapterType, 'name'> & ChapterBookmarkInfo & ChapterRealUrlInfo;
+}
