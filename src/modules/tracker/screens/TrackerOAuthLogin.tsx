Suwayomi/Suwayomi-WebUI

commit cff0ed5ec3f77a5961fbff2975522680e9afd6e9

Extract app routes into constant

src/modules/tracker/screens/TrackerOAuthLogin.tsx
@@ -11,6 +11,7 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { requestManager } from '@/lib/requests/RequestManager.ts';
 import { makeToast } from '@/modules/core/utils/Toast.ts';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 export const TrackerOAuthLogin = () => {
     const { t } = useTranslation();
@@ -38,7 +39,7 @@ export const TrackerOAuthLogin = () => {
                 makeToast(t('tracking.action.login.label.failure', { name: trackerName }), 'error');
             }
 
-            navigate('/settings/tracking', { replace: true });
+            navigate(AppRoutes.tracker.path, { replace: true });
         };
 
         login();

src/App.tsx
@@ -22,6 +22,7 @@ import { lazyLoadFallback } from '@/modules/core/utils/LazyLoad.tsx';
 import { ErrorBoundary } from '@/modules/core/components/ErrorBoundary.tsx';
 import { useNavBarContext } from '@/modules/navigation-bar/contexts/NavbarContext.tsx';
 import { Reader } from '@/modules/reader/screens/Reader.tsx';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 const { Browse } = loadable(() => import('@/modules/browse/screens/Browse.tsx'), lazyLoadFallback);
 const { DownloadQueue } = loadable(() => import('@/modules/downloads/screens/DownloadQueue.tsx'), lazyLoadFallback);
@@ -113,48 +114,51 @@ const MainApp = () => {
             <ErrorBoundary>
                 <Routes>
                     {/* General Routes */}
-                    <Route path="/" element={<Navigate to="/library" replace />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                    <Route path="settings">
+                    <Route path={AppRoutes.root.match} element={<Navigate to={AppRoutes.library.path} replace />} />
+                    <Route path={AppRoutes.matchAll.match} element={<Navigate to={AppRoutes.root.path} replace />} />
+                    <Route path={AppRoutes.settings.match}>
                         <Route index element={<Settings />} />
-                        <Route path="about" element={<About />} />
-                        <Route path="categories" element={<CategorySettings />} />
-                        <Route path="reader" element={<GlobalReaderSettings />} />
-                        <Route path="library">
+                        <Route path={AppRoutes.settings.childRoutes.about.match} element={<About />} />
+                        <Route path={AppRoutes.settings.childRoutes.categories.match} element={<CategorySettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.reader.match} element={<GlobalReaderSettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.library.match}>
                             <Route index element={<LibrarySettings />} />
-                            <Route path="duplicates" element={<LibraryDuplicates />} />
+                            <Route
+                                path={AppRoutes.settings.childRoutes.library.childRoutes.duplicates.match}
+                                element={<LibraryDuplicates />}
+                            />
                         </Route>
-                        <Route path="download" element={<DownloadSettings />} />
-                        <Route path="backup" element={<Backup />} />
-                        <Route path="server" element={<ServerSettings />} />
-                        <Route path="webui" element={<WebUISettings />} />
-                        <Route path="browse" element={<BrowseSettings />} />
-                        <Route path="device" element={<DeviceSetting />} />
-                        <Route path="tracking" element={<TrackingSettings />} />
-                        <Route path="appearance" element={<Appearance />} />
+                        <Route path={AppRoutes.settings.childRoutes.download.match} element={<DownloadSettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.backup.match} element={<Backup />} />
+                        <Route path={AppRoutes.settings.childRoutes.server.match} element={<ServerSettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.webui.match} element={<WebUISettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.browse.match} element={<BrowseSettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.device.match} element={<DeviceSetting />} />
+                        <Route path={AppRoutes.settings.childRoutes.tracking.match} element={<TrackingSettings />} />
+                        <Route path={AppRoutes.settings.childRoutes.appearance.match} element={<Appearance />} />
                     </Route>
 
                     {/* Manga Routes */}
 
-                    <Route path="sources">
-                        <Route index element={<Navigate to="/" replace />} />
-                        <Route path=":sourceId" element={<SourceMangas />} />
-                        <Route path=":sourceId/configure/" element={<SourceConfigure />} />
-                        <Route path="all/search/" element={<SearchAll />} />
+                    <Route path={AppRoutes.sources.match}>
+                        <Route index element={<Navigate to={AppRoutes.root.path} replace />} />
+                        <Route path={AppRoutes.sources.childRoutes.browse.match} element={<SourceMangas />} />
+                        <Route path={AppRoutes.sources.childRoutes.configure.match} element={<SourceConfigure />} />
+                        <Route path={AppRoutes.sources.childRoutes.searchAll.match} element={<SearchAll />} />
                     </Route>
-                    <Route path="downloads" element={<DownloadQueue />} />
-                    <Route path="manga/:id">
-                        <Route path="chapter/:chapterNum" element={null} />
+                    <Route path={AppRoutes.downloads.match} element={<DownloadQueue />} />
+                    <Route path={AppRoutes.manga.match}>
+                        <Route path={AppRoutes.manga.childRoutes.reader.match} element={null} />
                         <Route index element={<Manga />} />
                     </Route>
-                    <Route path="library" element={<Library />} />
-                    <Route path="updates" element={<Updates />} />
-                    <Route path="browse" element={<Browse />} />
-                    <Route path="migrate/source/:sourceId">
+                    <Route path={AppRoutes.library.match} element={<Library />} />
+                    <Route path={AppRoutes.updates.match} element={<Updates />} />
+                    <Route path={AppRoutes.browse.match} element={<Browse />} />
+                    <Route path={AppRoutes.migrate.match}>
                         <Route index element={<Migrate />} />
-                        <Route path="manga/:mangaId/search" element={<SearchAll />} />
+                        <Route path={AppRoutes.migrate.childRoutes.search.match} element={<SearchAll />} />
                     </Route>
-                    <Route path="tracker/login/oauth" element={<TrackerOAuthLogin />} />
+                    <Route path={AppRoutes.tracker.match} element={<TrackerOAuthLogin />} />
                 </Routes>
             </ErrorBoundary>
         </Box>
@@ -164,7 +168,7 @@ const MainApp = () => {
 const ReaderApp = () => (
     <ErrorBoundary>
         <Routes>
-            <Route path="*" element={<Reader />} />
+            <Route path={AppRoutes.matchAll.match} element={<Reader />} />
         </Routes>
     </ErrorBoundary>
 );
@@ -181,8 +185,8 @@ export const App: React.FC = () => (
                 <DefaultNavBar />
             </Box>
             <Routes>
-                <Route path="*" element={<MainApp />} />
-                <Route path="manga/:mangaId/chapter/:chapterIndex/*" element={<ReaderApp />} />
+                <Route path={AppRoutes.matchAll.match} element={<MainApp />} />
+                <Route path={AppRoutes.reader.match} element={<ReaderApp />} />
             </Routes>
         </Box>
     </AppContext>

src/modules/app-updates/components/ServerUpdateChecker.tsx
@@ -20,6 +20,7 @@ import { VersionUpdateInfoDialog } from '@/modules/app-updates/components/Versio
 import { useMetadataServerSettings } from '@/modules/settings/services/ServerSettingsMetadata.ts';
 import { useLocalStorage } from '@/modules/core/hooks/useStorage.tsx';
 import { getVersion } from '@/modules/app-updates/services/AppUpdateChecker.tsx';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 const disabledUpdateCheck = () => Promise.resolve();
 
@@ -87,7 +88,7 @@ export const ServerUpdateChecker = () => {
             return null;
         }
 
-        const isAboutPage = window.location.pathname === '/settings/about';
+        const isAboutPage = window.location.pathname === AppRoutes.settings.childRoutes.about.path;
         if (isAboutPage) {
             return null;
         }

src/modules/backup/screens/Backup.tsx
@@ -35,6 +35,7 @@ import { LoadingPlaceholder } from '@/modules/core/components/placeholder/Loadin
 import { EmptyViewAbsoluteCentered } from '@/modules/core/components/placeholder/EmptyViewAbsoluteCentered.tsx';
 import { defaultPromiseErrorHandler } from '@/lib/DefaultPromiseErrorHandler.ts';
 import { ServerSettings } from '@/modules/settings/Settings.types.ts';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 type BackupSettingsType = Pick<ServerSettings, 'backupPath' | 'backupTime' | 'backupInterval' | 'backupTTL'>;
 
@@ -357,7 +358,7 @@ export function Backup() {
                             <Button
                                 onClick={closeInvalidBackupDialog}
                                 component={Link}
-                                to="/browse"
+                                to={AppRoutes.browse.path}
                                 autoFocus={!!validationResult?.missingSources.length}
                                 variant={validationResult?.missingSources.length ? 'contained' : 'text'}
                             >
@@ -368,7 +369,7 @@ export function Backup() {
                             <Button
                                 onClick={closeInvalidBackupDialog}
                                 component={Link}
-                                to="/settings/tracking"
+                                to={AppRoutes.tracker.path}
                                 autoFocus={!!validationResult?.missingTrackers.length}
                                 variant={validationResult?.missingTrackers.length ? 'contained' : 'text'}
                             >

src/modules/category/components/CategorySelect.tsx
@@ -33,6 +33,7 @@ import {
 } from '@/lib/graphql/generated/graphql.ts';
 import { GET_CATEGORIES_BASE } from '@/lib/graphql/queries/CategoryQuery.ts';
 import { GET_MANGA_CATEGORIES } from '@/lib/graphql/queries/MangaQuery.ts';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 type BaseProps = {
     open: boolean;
@@ -232,7 +233,7 @@ export function CategorySelect(props: CategorySelectProps) {
                             width: '100%',
                         }}
                     >
-                        <Button component={Link} to="/settings/categories">
+                        <Button component={Link} to={AppRoutes.settings.childRoutes.categories.path}>
                             {t(allCategories.length ? 'global.button.edit' : 'global.button.create')}
                         </Button>
                         <Stack direction="row">

src/modules/chapter/services/Chapters.ts
@@ -22,6 +22,7 @@ import { CHAPTER_LIST_FIELDS } from '@/lib/graphql/fragments/ChapterFragments.ts
 import { DirectionOffset, TranslationKey } from '@/Base.types.ts';
 import { MangaIdInfo } from '@/modules/manga/Manga.types.ts';
 import { ReaderResumeMode } from '@/modules/reader/types/Reader.types.ts';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 export type ChapterAction = 'download' | 'delete' | 'bookmark' | 'unbookmark' | 'mark_as_read' | 'mark_as_unread';
 
@@ -118,7 +119,7 @@ export class Chapters {
     }
 
     static getReaderUrl<Chapter extends ChapterMangaInfo & ChapterSourceOrderInfo>(chapter: Chapter): string {
-        return `/manga/${chapter.mangaId}/chapter/${chapter.sourceOrder}`;
+        return AppRoutes.reader.path(chapter.mangaId, chapter.sourceOrder);
     }
 
     static isDownloading(id: number): boolean {

src/modules/core/AppRoute.constants.ts
@@ -1,0 +1,157 @@
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { SourceType } from '@/lib/graphql/generated/graphql.ts';
+import { MangaIdInfo } from '@/modules/manga/Manga.types.ts';
+import { ChapterSourceOrderInfo } from '@/modules/chapter/services/Chapters.ts';
+
+type AppRouteInfo = {
+    match: string;
+    path?: string | ((...args: any[]) => string);
+};
+
+type TAppRoutes = Record<string, AppRouteInfo & { childRoutes?: TAppRoutes }>;
+
+export const AppRoutes = {
+    root: {
+        match: '/',
+        path: '/',
+    },
+    matchAll: {
+        match: '*',
+    },
+    settings: {
+        path: '/settings',
+        match: 'settings',
+        childRoutes: {
+            about: {
+                match: 'about',
+                path: '/settings/about',
+            },
+            categories: {
+                match: 'categories',
+                path: '/settings/categories',
+            },
+            reader: {
+                match: 'reader',
+                path: '/settings/reader',
+            },
+            library: {
+                match: 'library',
+                path: '/settings/library',
+
+                childRoutes: {
+                    duplicates: {
+                        match: 'duplicates',
+                        path: '/settings/library/duplicates',
+                    },
+                },
+            },
+            download: {
+                match: 'download',
+                path: '/settings/download',
+            },
+            backup: {
+                match: 'backup',
+                path: '/settings/backup',
+            },
+            server: {
+                match: 'server',
+                path: '/settings/server',
+            },
+            webui: {
+                match: 'webui',
+                path: '/settings/webui',
+            },
+            browse: {
+                match: 'browse',
+                path: '/settings/browse',
+            },
+            device: {
+                match: 'device',
+                path: '/settings/device',
+            },
+            tracking: {
+                match: 'tracking',
+                path: '/settings/tracking',
+            },
+            appearance: {
+                match: 'appearance',
+                path: '/settings/appearance',
+            },
+        },
+    },
+    sources: {
+        match: 'sources',
+        path: '/sources',
+
+        childRoutes: {
+            browse: {
+                match: ':sourceId',
+                path: (sourceId: SourceType['id']) => `/sources/${sourceId}`,
+            },
+            configure: {
+                match: ':sourceId/configure',
+                path: (sourceId: SourceType['id']) => `/sources/${sourceId}/configure`,
+            },
+            searchAll: {
+                match: 'all/search',
+                path: '/sources/all/search',
+            },
+        },
+    },
+    downloads: {
+        match: 'downloads',
+        path: '/downloads',
+    },
+    manga: {
+        match: 'manga/:id',
+        path: (mangaId: MangaIdInfo['id']) => `/manga/${mangaId}`,
+
+        childRoutes: {
+            reader: {
+                match: 'chapter/:chapterNum',
+                path: (mangaId: MangaIdInfo['id'], chapterNum: ChapterSourceOrderInfo['sourceOrder']) =>
+                    `/manga/${mangaId}/chapter/${chapterNum}`,
+            },
+        },
+    },
+    library: {
+        match: 'library',
+        path: '/library',
+    },
+    updates: {
+        match: 'updates',
+        path: '/updates',
+    },
+    browse: {
+        match: 'browse',
+        path: '/browse',
+    },
+    migrate: {
+        match: 'migrate/source/:sourceId',
+        path: (sourceId: SourceType['id']) => `/migrate/source/${sourceId}`,
+
+        childRoutes: {
+            search: {
+                match: 'manga/:mangaId/search',
+                path: (sourceId: SourceType['id'], mangaId: MangaIdInfo['id']) =>
+                    `/migrate/source/${sourceId}/manga/${mangaId}/search`,
+            },
+        },
+    },
+    tracker: {
+        match: 'tracker/login/oauth',
+        path: '/tracker/login/oauth',
+    },
+    reader: {
+        match: '/manga/:mangaId/chapter/:chapterIndex',
+        path: (mangaId: MangaIdInfo['id'], chapterIndex: ChapterSourceOrderInfo['sourceOrder']) =>
+            `/manga/${mangaId}/chapter/${chapterIndex}`,
+    },
+} satisfies TAppRoutes;

src/modules/core/hooks/useBackButton.ts
@@ -8,6 +8,7 @@
 
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useNavBarContext } from '@/modules/navigation-bar/contexts/NavbarContext.tsx';
+import { AppRoutes } from '@/modules/core/AppRoute.constants.ts';
 
 const PAGES_TO_IGNORE: readonly RegExp[] = [/\/manga\/[0-9]+\/chapter\/[0-9]+/g];
 
@@ -27,6 +28,6 @@ export const useBackButton = () => {
             return;
         }
 
-        navigate('/library');
+        navigate(AppRoutes.library.path);
     };
 };

commit 34f484a946a351e3db4fb70460daf8a32a6e32b6

Redirect to correct page after track oauth login
Regression introduced with cff0ed5ec3f77a5961fbff2975522680e9afd6e9

src/modules/tracker/screens/TrackerOAuthLogin.tsx
@@ -40,7 +40,7 @@ export const TrackerOAuthLogin = () => {
                 makeToast(t('tracking.action.login.label.failure', { name: trackerName }), 'error', getErrorMessage(e));
             }
 
-            navigate(AppRoutes.tracker.path, { replace: true });
+            navigate(AppRoutes.settings.childRoutes.tracking.path, { replace: true });
         };
 
         login();