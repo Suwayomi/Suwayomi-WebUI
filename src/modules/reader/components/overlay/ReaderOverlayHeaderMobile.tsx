056db542df249e90dd5580ff0573b4f26060b2fc
schroda <50052685+schroda@users.noreply.github.com>
2024-12-22 21:59:28 +0100

    Add missing padding top in reader mobile header
    Regression d844ad3f5d7a7ebcbff5fd41fcded314113ce1af

--- src/modules/reader/components/overlay/ReaderOverlayHeaderMobile.tsx
+++ src/modules/reader/components/overlay/ReaderOverlayHeaderMobile.tsx
@@ -76,7 +76,7 @@ const BaseReaderOverlayHeaderMobile = ({
                     left: 0,
                     right: `${scrollbarYSize}px`,
                     p: 2,
-                    pt: (theme) => `env(safe-area-inset-top, ${theme.spacing(2)})`,
+                    pt: (theme) => `max(env(safe-area-inset-top), ${theme.spacing(2)})`,
                     backgroundColor: (theme) => alpha(theme.palette.background.paper, 0.95),
                     pointerEvents: 'all',
                 }}
