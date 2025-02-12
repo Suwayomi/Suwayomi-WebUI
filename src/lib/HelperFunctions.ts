ffd6c5867fbbeeb19f12574f818863c8e5a8216a
schroda <50052685+schroda@users.noreply.github.com>
2025-02-12 19:53:05 +0100

    Fix "coerceIn" for calls with max < min
    Previously the min value was ignored in case the max value was less than the min value.
    E.g. coerceIn(5, 0, -1) would be -1 instead of 0

--- src/lib/HelperFunctions.ts
+++ src/lib/HelperFunctions.ts
@@ -38,6 +38,6 @@ export const getValueFromObject = <T>(obj: Record<string, any>, key: string): T 
     return keys.reduce((acc, curr) => acc?.[curr], obj) as T;
 };
 
-export const coerceIn = (value: number, min: number, max: number): number => Math.min(Math.max(value, min), max);
+export const coerceIn = (value: number, min: number, max: number): number => Math.max(Math.min(value, max), min);
 
 export const noOp = () => {};
