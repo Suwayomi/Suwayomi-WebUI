0db68c8a9ae2b910f7fc28e3e555cd57d2de5dfa
NamNH <namitonguyen@gmail.com>
2025-03-05 21:21:52 +0700

    Refactor History screen to remove unused components.
    Removed unused states, layout effects, and props related to last update timestamp. Renamed variables for better clarity and updated labels and error messages to align with the history context. Simplified component structure for improved maintainability.

--- src/modules/history/screens/History.tsx
+++ src/modules/history/screens/History.tsx
@@ -7,7 +7,7 @@
  */
 
 import Typography from '@mui/material/Typography';
-import React, { useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useLayoutEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { requestManager } from '@/lib/requests/RequestManager.ts';
 import { LoadingPlaceholder } from '@/modules/core/components/placeholder/LoadingPlaceholder.tsx';
@@ -17,7 +17,7 @@ import { UpdateChecker } from '@/modules/core/components/UpdateChecker.tsx';
 import { StyledGroupedVirtuoso } from '@/modules/core/components/virtuoso/StyledGroupedVirtuoso.tsx';
 import { StyledGroupHeader } from '@/modules/core/components/virtuoso/StyledGroupHeader.tsx';
 import { StyledGroupItemWrapper } from '@/modules/core/components/virtuoso/StyledGroupItemWrapper.tsx';
-import { dateTimeFormatter, epochToDate, getDateString } from '@/util/DateHelper.ts';
+import { epochToDate, getDateString } from '@/util/DateHelper.ts';
 import { defaultPromiseErrorHandler } from '@/lib/DefaultPromiseErrorHandler.ts';
 import { VirtuosoUtil } from '@/lib/virtuoso/Virtuoso.util.tsx';
 import { getErrorMessage } from '@/lib/HelperFunctions.ts';
@@ -54,46 +54,32 @@ export const History: React.FC = () => {
     });
     const hasNextPage = !!chapterHistoryData?.chapters.pageInfo.hasNextPage;
     const endCursor = chapterHistoryData?.chapters.pageInfo.endCursor;
-    const updateEntries = chapterHistoryData?.chapters.nodes ?? [];
-    const groupedHistory = useMemo(() => groupByDate(updateEntries), [updateEntries]);
+    const readEntries = chapterHistoryData?.chapters.nodes ?? [];
+    const groupedHistory = useMemo(() => groupByDate(readEntries), [readEntries]);
     const groupCounts: number[] = useMemo(() => groupedHistory.map((group) => group[1]), [groupedHistory]);
 
     const computeItemKey = VirtuosoUtil.useCreateGroupedComputeItemKey(
         groupCounts,
         useCallback((index) => groupedHistory[index][0], [groupedHistory]),
-        useCallback((index) => updateEntries[index].id, [updateEntries]),
+        useCallback((index) => readEntries[index].id, [readEntries]),
     );
 
-    const lastUpdateTimestampCompRef = useRef<HTMLElement>(null);
-    const [lastUpdateTimestampCompHeight, setLastUpdateTimestampCompHeight] = useState(0);
     useLayoutEffect(() => {
-        setLastUpdateTimestampCompHeight(lastUpdateTimestampCompRef.current?.clientHeight ?? 0);
-    }, [lastUpdateTimestampCompRef.current]);
-
-    const { data: lastUpdateTimestampData } = requestManager.useGetLastGlobalUpdateTimestamp({
-        /**
-         * The {@link UpdateChecker} is responsible for updating the timestamp
-         */
-        fetchPolicy: 'cache-only',
-    });
-    const lastUpdateTimestamp = lastUpdateTimestampData?.lastUpdateTimestamp.timestamp;
-
-    useLayoutEffect(() => {
-        setTitle(t('updates.title'));
+        setTitle(t('history.title'));
         setAction(<UpdateChecker />);
 
         return () => {
             setTitle('');
             setAction(null);
         };
-    }, [t, lastUpdateTimestamp]);
+    }, [t]);
 
     const loadMore = useCallback(() => {
         if (!hasNextPage) {
             return;
         }
 
-        fetchMore({ variables: { offset: updateEntries.length } });
+        fetchMore({ variables: { offset: readEntries.length } });
     }, [hasNextPage, endCursor]);
 
     if (error) {
@@ -101,54 +87,40 @@ export const History: React.FC = () => {
             <EmptyViewAbsoluteCentered
                 message={t('global.error.label.failed_to_load_data')}
                 messageExtra={getErrorMessage(error)}
-                retry={() => refetch().catch(defaultPromiseErrorHandler('Updates::refetch'))}
+                retry={() => refetch().catch(defaultPromiseErrorHandler('History::refetch'))}
             />
         );
     }
 
-    if (!isLoading && updateEntries.length === 0) {
-        return <EmptyViewAbsoluteCentered message={t('updates.error.label.no_updates_available')} />;
+    if (!isLoading && readEntries.length === 0) {
+        return <EmptyViewAbsoluteCentered message={t('history.error.label.no_history_available')} />;
     }
 
     return (
-        <>
-            <Typography
-                ref={lastUpdateTimestampCompRef}
-                sx={{
-                    marginLeft: '10px',
-                    paddingTop: (theme) => ({ [theme.breakpoints.up('sm')]: { paddingTop: '6px' } }),
-                }}
-            >
-                {t('library.settings.global_update.label.last_update', {
-                    date: lastUpdateTimestamp ? dateTimeFormatter.format(+lastUpdateTimestamp) : '-',
-                })}
-            </Typography>
-            <StyledGroupedVirtuoso
-                heightToSubtract={lastUpdateTimestampCompHeight}
-                style={{
-                    // override Virtuoso default values and set them with class
-                    height: 'undefined',
-                }}
-                components={{
-                    Footer: () => (isLoading ? <LoadingPlaceholder usePadding /> : null),
-                }}
-                overscan={window.innerHeight * 0.5}
-                endReached={loadMore}
-                groupCounts={groupCounts}
-                groupContent={(index) => (
-                    <StyledGroupHeader isFirstItem={index === 0}>
-                        <Typography variant="h5" component="h2">
-                            {groupedHistory[index][0]}
-                        </Typography>
-                    </StyledGroupHeader>
-                )}
-                computeItemKey={computeItemKey}
-                itemContent={(index) => (
-                    <StyledGroupItemWrapper>
-                        <ChapterHistoryCard chapter={updateEntries[index]} />
-                    </StyledGroupItemWrapper>
-                )}
-            />
-        </>
+        <StyledGroupedVirtuoso
+            style={{
+                // override Virtuoso default values and set them with class
+                height: 'undefined',
+            }}
+            components={{
+                Footer: () => (isLoading ? <LoadingPlaceholder usePadding /> : null),
+            }}
+            overscan={window.innerHeight * 0.5}
+            endReached={loadMore}
+            groupCounts={groupCounts}
+            groupContent={(index) => (
+                <StyledGroupHeader isFirstItem={index === 0}>
+                    <Typography variant="h5" component="h2">
+                        {groupedHistory[index][0]}
+                    </Typography>
+                </StyledGroupHeader>
+            )}
+            computeItemKey={computeItemKey}
+            itemContent={(index) => (
+                <StyledGroupItemWrapper>
+                    <ChapterHistoryCard chapter={readEntries[index]} />
+                </StyledGroupItemWrapper>
+            )}
+        />
     );
 };
