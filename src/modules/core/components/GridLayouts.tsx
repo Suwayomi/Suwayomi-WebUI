c81dce8efe38db3e66560dab8a1f2d2a4ea9063d
schroda <50052685+schroda@users.noreply.github.com>
2025-03-28 19:06:08 +0100

    Remove margin between icons in "extensions actions"

--- src/modules/core/components/GridLayouts.tsx
+++ src/modules/core/components/GridLayouts.tsx
@@ -47,7 +47,6 @@ export function GridLayouts({
                 <IconButton
                     onClick={handleClick}
                     size="small"
-                    sx={{ ml: 2 }}
                     aria-controls={open ? 'account-menu' : undefined}
                     aria-haspopup="true"
                     aria-expanded={open ? 'true' : undefined}
