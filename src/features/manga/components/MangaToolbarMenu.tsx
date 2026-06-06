646907fa199c75c90c985b64a7f2777d1033a42f
schroda <50052685+schroda@users.noreply.github.com>
2026-06-06 17:54:56 +0200

    Fix single migration manga page mobile entrypoint
    Regression 278ebb312f3d9f7339db464323b2fa388d56c40d

--- src/features/manga/components/MangaToolbarMenu.tsx
+++ src/features/manga/components/MangaToolbarMenu.tsx
@@ -173,6 +173,7 @@ export const MangaToolbarMenu = ({ manga, onRefresh, refreshing }: IProps) => {
                                 )}
                                 state={AppRoutes.migrate.children.singleMangaSearch.state({
                                     title: t`Migrate "${manga.title}"`,
+                                    mode: 'migrate.select.single',
                                 })}
                                 style={{ textDecoration: 'none', color: 'inherit' }}
                             >
