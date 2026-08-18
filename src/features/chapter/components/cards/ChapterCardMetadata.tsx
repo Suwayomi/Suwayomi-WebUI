0b6b533d299edfe2cbbfb766844085efd4dece02
schroda <50052685+schroda@users.noreply.github.com>
2026-08-18 23:52:09 +0200

    Show chapter card text tooltip to the right
    They were shown below the text by default. This messed with showing the tooltip for the text below the currently hovered text.

--- src/features/chapter/components/cards/ChapterCardMetadata.tsx
+++ src/features/chapter/components/cards/ChapterCardMetadata.tsx
@@ -66,26 +66,29 @@ export const ChapterCardMetadata = ({
             }}
         >
             {infoIcons}
-            <CustomTooltip title={title} {...TOOLTIP_PREVENT_OVERFLOW}>
+            <CustomTooltip title={title} placement="right" {...TOOLTIP_PREVENT_OVERFLOW}>
                 <TypographyMaxLines variant="h6" component="h3" {...slotProps?.title}>
                     {title}
                 </TypographyMaxLines>
             </CustomTooltip>
         </Stack>
         {secondaryText && (
-            <CustomTooltip title={secondaryText} {...TOOLTIP_PREVENT_OVERFLOW}>
+            <CustomTooltip title={secondaryText} placement="right" {...TOOLTIP_PREVENT_OVERFLOW}>
                 <TypographyMaxLines
                     variant="caption"
                     lines={1}
                     {...slotProps?.secondaryText}
-                    sx={{ width: 'fit-content', ...slotProps?.secondaryText?.sx }}
+                    sx={{
+                        width: 'fit-content',
+                        ...slotProps?.secondaryText?.sx,
+                    }}
                 >
                     {secondaryText}
                 </TypographyMaxLines>
             </CustomTooltip>
         )}
         {ternaryText && (
-            <CustomTooltip title={ternaryText} {...TOOLTIP_PREVENT_OVERFLOW}>
+            <CustomTooltip title={ternaryText} placement="right" {...TOOLTIP_PREVENT_OVERFLOW}>
                 <TypographyMaxLines
                     variant="caption"
                     lines={1}
